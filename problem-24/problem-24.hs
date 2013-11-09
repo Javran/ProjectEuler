acde02f0c9049c80a98621044af9478d8d86d723
Javran Cheng <Javran.c@gmail.com>
2013-11-09 12:21:24 -0500

    problem 24

---
+++ problem-24/problem-24.hs
@@ -0,0 +1,25 @@
+import Data.Char
+factorial n = product [1..n]
+
+-- define `permutation id` x to be the identity of the (x+1)-th permutaion
+-- e.g. 0,1,2, pid 0 => 0,1,2, pid 1 => 0,2,1, ... etc.
+-- the problem is to find the (999999+1)-th permutation for [0..9], pid = 999999
+pidToPermutation :: Int -> [Int] -> [Int]
+pidToPermutation 0 arr = arr
+pidToPermutation pid arr = h:(pidToPermutation subPid t)
+    where
+        n = length arr
+        subCycle = factorial $ n - 1
+        headInd = pid `div` subCycle
+        (h,t) = breakList headInd arr
+        subPid = pid `mod` subCycle
+
+-- get the x-indexed element, return the list after removing that element
+breakList :: (Eq a) => Int -> [a] -> (a,[a])
+breakList 0 (x:xs) = (x,xs)
+breakList n (x:xs) = (x1,x:xs1)
+    where
+        (x1,xs1) = breakList (n-1) xs
+
+main = do
+    print $ map (\x -> chr $ ord '0' + x) $ pidToPermutation 999999 [0..9] 
