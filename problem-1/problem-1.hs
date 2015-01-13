26dda37e8ea9c89e7de110a05c727341c6648754
Javran Cheng <Javran.c@gmail.com>
2015-01-13 02:44:01 -0500

    problem 1 & 2 - improve code

--- problem-1/problem-1.hs
+++ problem-1/problem-1.hs
@@ -1,1 +1,6 @@
-main = print $ sum [ x | x <-[1..999], x `mod` 3 == 0 || x `mod` 5 == 0 ]
+main :: IO ()
+main = print
+     $ sum [ x
+           | x <- [1::Int .. 999]
+           , x `mod` 3 == 0 || x `mod` 5 == 0
+           ]
--- problem-2/problem-2.hs
+++ problem-2/problem-2.hs
@@ -1,4 +1,8 @@
 fibs :: [Int]
-fibs = map snd $ iterate (\(x,y) -> (y,x+y)) (1,1)
+fibs = 0:1:zipWith (+) fibs (tail fibs)
 
-main = print $ sum $ filter even $ takeWhile (<=4000000) fibs
+main :: IO ()
+main = print
+     . sum . filter even      -- sum of evens
+     . takeWhile (<=4000000)  -- do not exceed four million
+     $ drop 2 fibs            -- correct the offset
