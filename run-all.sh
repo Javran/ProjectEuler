b7be15c330ea8048d3b246c83939b9f31da717e5
Javran Cheng <Javran.c@gmail.com>
2015-01-14 15:41:07 -0500

    run script

--- run-all.sh
+++ run-all.sh
@@ -5,12 +5,14 @@
 mkdir -p _build
 rm -rf _build/*
 
-for PROBLEM_SRC in src/Problem-*.hs
+for PROBLEM_SRC in `ls src/Problem-*.hs | sort -V`
 do
     PROBLEM=${PROBLEM_SRC#src/}
     PROBLEM=${PROBLEM/.hs/}
     echo Compiling $PROBLEM ...
-    ghc -O2 $PROBLEM_SRC -o _build/${PROBLEM}.bin
+    ghc -O2 $PROBLEM_SRC -o _build/${PROBLEM}.bin -outputdir=_build -fforce-recomp
+    cd _build
 
-    timeout 5s /usr/bin/time -f "%e" ./_build/${PROBLEM}.bin
+    timeout 5s /usr/bin/time -f "%e" ./${PROBLEM}.bin
+    cd ..
 done
