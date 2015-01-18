c420f5d27c29fd4287a3f71e0c7d360429dbcf90
Javran Cheng <Javran.c@gmail.com>
2015-01-18 00:25:04 -0500

    Problem-14 - this one's fast enough

--- solutions/Problem-14.hs
+++ solutions/Problem-14.hs
@@ -2,29 +2,38 @@ import Control.Applicative
 import Control.Arrow
 import Control.Monad.State
 import qualified Data.Map.Strict as M
+import Data.Int
+import Data.Array
+import Data.Array.ST
+import Control.Monad.ST
+import Data.Function
+import Data.List
 
-collatz :: Integer -> Integer
+collatz :: Integral a => a -> a
 collatz n = if odd n
               then 3 * n + 1
               else n `div` 2
 
-memoCollatz :: Integer
-            -> State (M.Map Integer Int, (Integer,Int)) Int
-memoCollatz 1 = return 1
-memoCollatz n = do
-    result <- gets (M.lookup n . fst)
-    case result of
-        Nothing -> do
-            l <- succ <$> memoCollatz (collatz n)
-            let update p@(_,curMaxV) =
-                    if l > curMaxV
-                       then (n,l)
-                       else p
-            modify (M.insert n l *** update)
-            return l
-        Just v -> return v
+limit :: Integral a => a
+limit = 1000000
+
+-- Int is sufficient as it's guaranteed to store at lease 2^29 - 1
+collatzArray :: Array Int Int
+collatzArray = runSTArray $ do
+    ar <- newArray (1,limit) 0
+    let calc 1 = return 1
+        calc n = if n > limit
+          then (+ 1) <$> calc (collatz n)
+          else do
+            v <- readArray ar n
+            if v /= 0
+               then return v
+               else do
+                 v' <- (+ 1) <$> calc (collatz n)
+                 writeArray ar n v'
+                 return v'
+    mapM_ calc [1..limit]
+    return ar
 
 main :: IO ()
-main = print $ snd (execState (mapM_ memoCollatz [1..limit]) (M.empty,(1,1)))
-  where
-    limit = 1000000
+main = print $ maximumBy (compare `on` snd) (assocs collatzArray)
