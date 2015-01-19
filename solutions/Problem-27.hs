168934e7d3795515d4b59d275254efba1d83df85
Javran Cheng <Javran.c@gmail.com>
2015-01-19 17:08:04 -0500

    Problem 27, 44 - performance

--- solutions/Problem-27.hs
+++ solutions/Problem-27.hs
@@ -5,24 +5,18 @@ import Control.Monad
 
 -- TODO: things after problem 27 are migrated without too much refactoring
 
+consePrimeLen :: Int -> Int -> Int
 consePrimeLen a b = length $ takeWhile isPrime [ n * n + a * n + b | n <- [0..] ]
 
+main :: IO ()
 main = do
-    let searchSpace = do
-        a <- [0..1000]
-        b <- takeWhile (<1000) primes
-        guard $ gcd a b == 1
-        c <-[-a,a]
-        d <-[-b,b]
-        guard $ c + d > 0
-        return (c,d)
-
+    let limitedPrimes = takeWhile (<1000) primes
+        searchSpace = do
+          a <- [0..1000 :: Int]
+          b <- limitedPrimes
+          guard $ gcd a b == 1
+          c <-[-a,a]
+          d <-[-b,b]
+          guard $ c + d > 0
+          return (c,d)
     print $ uncurry (*) $ maximumBy (compare `on` uncurry consePrimeLen) searchSpace
-
-{-
-compile with: ghc -O2 problem-27.hs -rtsopts
-run with: ./problem-27 +RTS -K200000000 -RTS
-real    0m19.284s
-user    0m18.859s
-sys     0m0.384s
--}
