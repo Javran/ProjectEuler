c6cd4eb01fefe2fe904fc439ad6c8df20a87ec4e
Javran Cheng <Javran.c@gmail.com>
2015-01-19 06:30:17 -0500

    Problem-39 - Integer to Int

--- solutions/Problem-39.hs
+++ solutions/Problem-39.hs
@@ -2,6 +2,7 @@ import Control.Monad
 import Data.List
 import Data.Function (on)
 
+solutions :: Int -> Int
 solutions p = length sol
     where
         sol = do
@@ -11,11 +12,5 @@ solutions p = length sol
             guard $ c*c == a*a + b*b
             return (a,b,c)
 
-main = print $ maximumBy (compare `on` solutions) [1..1000]
-
-{-
-compile with: ghc problem-39.hs -O2
-real    0m25.338s
-user    0m24.536s
-sys     0m0.700s
--}
+main :: IO ()
+main = print $ maximumBy (compare `on` solutions) [1..1000 :: Int]
