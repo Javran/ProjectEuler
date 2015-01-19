10cae393190bb06cccaebef9345d6f8d469484b0
Javran Cheng <Javran.c@gmail.com>
2015-01-19 06:51:16 -0500

    Problem-23 - slightly better ..

--- solutions/Problem-23.hs
+++ solutions/Problem-23.hs
@@ -1,4 +1,5 @@
 import Data.List
+import qualified Data.List.Ordered as LO
 
 -- TODO: need some work, skipping for now
 
@@ -13,10 +14,10 @@ sys     0m0.520s
 -- return a list of divisors of x
 divisors :: Int -> [Int]
 -- guaranteed that the last one should be x, remove it safely
-divisors x = init $ sort $ union leftHalf rightHalf
+divisors x = init $ sort $ LO.union leftHalf rightHalf
     where
         leftHalf = [ y | y <- [1..x], y * y <= x, x `mod` y == 0]
-        rightHalf = [x `div` y | y <- leftHalf]
+        rightHalf = reverse [x `div` y | y <- leftHalf]
 
 isAbundant :: Int -> Bool
 isAbundant n = n < sum (divisors n)
@@ -26,21 +27,12 @@ removeDup :: [Int] -> [Int]
 removeDup (x:xs) = reverse $ foldl (\acc i -> if head acc == i then acc else i:acc) [x] xs
 removeDup [] = []
 
--- x \\ y, assuming x and y are all sorted
-diffSorted :: [Int] -> [Int] -> [Int]
-diffSorted [] _ = []
-diffSorted xs [] = xs
-diffSorted (x:xs) (y:ys)
-    | x > y     = diffSorted (x:xs) ys
-    | x < y     = x:diffSorted xs (y:ys)
-    | otherwise = diffSorted xs (y:ys) 
-
 maxAbun = 28123
 
 main = do
     let possibleAbuns = takeWhile (<= maxAbun) $ filter isAbundant [1..]
     let reachables = sort $ do
-        x <- possibleAbuns
-        y <- takeWhile (\u -> u + x <= maxAbun) possibleAbuns
-        return (x+y)
-    print $ sum $ diffSorted [1..maxAbun] $ removeDup reachables
+          x <- possibleAbuns
+          y <- takeWhile (\u -> u + x <= maxAbun) possibleAbuns
+          return (x+y)
+    print $ sum $ LO.minus [1..maxAbun] $ LO.nub reachables
