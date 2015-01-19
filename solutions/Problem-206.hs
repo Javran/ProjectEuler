7d37ee1689678cb97a82df478377c428ae59d255
Javran Cheng <Javran.c@gmail.com>
2015-01-19 06:24:02 -0500

    Problem-206 - improved

--- solutions/Problem-206.hs
+++ solutions/Problem-206.hs
@@ -1,39 +1,33 @@
 import Data.Char
 import Data.Int
+import Petbox
 
--- from: http://www.haskell.org/haskellwiki/Generic_number_type#squareRoot
+squareRoot :: Int64 -> Int64
+squareRoot =  floor . (sqrt :: Double -> Double) . fromIntegral
 
-(^!) :: Num a => a -> Int -> a
-(^!) x n = x^n
+searchSpaceDiv10 :: [Int64]
+searchSpaceDiv10 = filter validEnd [lBound .. uBound]
+  where
+    -- let's say this perfect square number is n*n = N
+    -- the last unknown digit of N must be '0',
+    -- because the last digit of N is '0',
+    -- to make N a perfect square, n must be divisible by 10
+    -- therefore the search space is dramatically reduced
+    lBound, uBound :: Int64
+    lBound = squareRoot . read $ "10203040506070809"
+    uBound = squareRoot . read $ "19293949596979899"
+    -- further, since N / 100 ends in '9',
+    -- the last digit of n must be either 3 or 7
+    validEnd n = (n `mod` 10) `elem` [3,7]
 
-squareRoot :: Integer -> Integer
-squareRoot 0 = 0
-squareRoot 1 = 1
-squareRoot n =
-   let twopows = iterate (^!2) 2
-       (lowerRoot, lowerN) =
-          last $ takeWhile ((n>=) . snd) $ zip (1:twopows) twopows
-       newtonStep x = div (x + div n x) 2
-       iters = iterate newtonStep (squareRoot (div n lowerN) * lowerRoot)
-       isRoot r  =  r^!2 <= n && n < (r+1)^!2
-   in  head $ dropWhile (not . isRoot) iters
-
-space :: [Int64]
-space = [1010101010..1389026624] 
-
-main = print $ squareRoot $ fromIntegral $ head $ filter f $ map (^2) space
-
-f x = (length xStr == length "1_2_3_4_5_6_7_8_9_0") && and (zipWith test xStr [1..])
+-- check the pattern
+valid :: Int64 -> Bool
+valid xDiv10 = and (zipWith test xStr [1..])
     where
-        xStr = show x
-        test ch ind
+      xStr = show (xDiv10 * xDiv10)
+      test ch ind
             | even ind = True
-            | ind == 19 = ch == '0'
             | otherwise = (ord ch - ord '0') == (ind + 1) `div` 2
 
-{-
-compile with: ghc -O
-real    2m26.044s
-user    2m25.843s
-sys     0m0.317s
--}
+main :: IO ()
+main = print . (*10) . firstSuchThat valid $ searchSpaceDiv10
