be27e3df6b36e4e53c8ac85e0115be52711e7731
Javran Cheng <Javran.c@gmail.com>
2019-06-09 22:42:24 -0700

    automate updating AllProblems.hs

--- ProjectEuler/templater/Main.hs
+++ ProjectEuler/templater/Main.hs
@@ -1,22 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Applicative
+import Data.Aeson
+import Data.Maybe
+import Data.Time.Clock
+import Filesystem.Path.CurrentOS ((</>))
+import Text.Microstache
+import Turtle.Pattern
 import Turtle.Prelude
 import Turtle.Shell
-import Turtle.Pattern
-import Data.Maybe
-import Text.Microstache
-import Data.Aeson
-import Control.Applicative
-import Data.Time.Clock
 
+import qualified Control.Foldl as Foldl
+import qualified Data.HashMap.Strict as HM
+import qualified Data.IntSet as IS
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
-import qualified Filesystem.Path.CurrentOS as FP
-import qualified Control.Foldl as Foldl
-import qualified Data.IntSet as IS
-import qualified Data.HashMap.Strict as HM
+import qualified Data.Text.Lazy.IO as TL
 import qualified Data.Vector as V
+import qualified Filesystem.Path.CurrentOS as FP
 
 {-
   The purpose of templater is to ... well, apply templates.
@@ -28,13 +30,15 @@ import qualified Data.Vector as V
 
   Usage: require environment variable "PROJECT_EULER_HOME" to point to project home directory.
 
+  TODO: plan to bring up "sync" functionality first and continue to allow creating
+  problem modules with template.
+
   - `templater <num>`: create Problem<num> using template (implies `sync`)
   - `templater sync`:
     + scan through problems and re-generate AllProblems.hs.
     + upate package.yaml and update the list of problem modules
 
  -}
-
 
 scanProblems :: FP.FilePath -> IO IS.IntSet
 scanProblems projectHome = do
@@ -68,14 +72,20 @@ renderAllProblemsContent t tmpl pIds = renderMustache tmpl ctxt
             , ("val", Number $ fromIntegral pId)
             ]
 
+updateAllProblems :: FP.FilePath -> IO ()
+updateAllProblems projectHome = do
+  let allProblemsTmplPath =
+        projectHome </> "templater" </> "mustache" </> "AllProblems.hs"
+      allProblemsFilePath =
+        projectHome </> "src" </> "ProjectEuler" </> "AllProblems.hs"
+  template <- compileMustacheFile (FP.encodeString allProblemsTmplPath)
+  pIds <- scanProblems projectHome
+  t <- getCurrentTime
+  let content = renderAllProblemsContent t template pIds
+  TL.writeFile (FP.encodeString allProblemsFilePath) content
+
 main :: IO ()
 main = do
   curEnv <- env
   let Just projectHome = FP.fromText <$> lookup "PROJECT_EULER_HOME" curEnv
-      allProblemsTmplPath =
-        projectHome FP.</> "templater" FP.</> "mustache" FP.</> "AllProblems.hs"
-  template <- compileMustacheFile (FP.encodeString allProblemsTmplPath)
-  pIds <- scanProblems projectHome
-  t <- getCurrentTime
-  let out = renderAllProblemsContent t template pIds
-  putStr (T.unpack . TL.toStrict $ out)
+  updateAllProblems projectHome
