2bf1eb0bad3704202cf43f04505ba63071e1a0ec
Javran Cheng <Javran.c@gmail.com>
2019-06-11 22:44:45 -0700

    measure time when running

--- ProjectEuler/src/Main.hs
+++ ProjectEuler/src/Main.hs
@@ -1,13 +1,27 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE
+    LambdaCase
+  , NamedFieldPuns
+  #-}
 module Main
   ( main
   ) where
 
-import System.Exit
-import System.Environment
 import qualified Data.IntMap.Strict as IM
-import ProjectEuler.Types
 import ProjectEuler.AllProblems
+import ProjectEuler.Types
+import System.CPUTime
+import System.Environment
+import System.Exit
+import Text.Printf
+
+evalProblem :: Problem -> IO ()
+evalProblem Problem {problemId, problemRun} = do
+  putStrLn $ "Evaluating Problem #" <> show problemId
+  tStart <- getCPUTime
+  problemRun []
+  tEnd <- getCPUTime
+  let diff = fromIntegral (tEnd - tStart) / (10^(9 :: Int))
+  printf "Time elapsed: %0.4f ms\n" (diff :: Double)
 
 {-
   main program is named "pet" for ProjectEuler Toolkit.
@@ -24,9 +38,7 @@ main = getArgs >>= \case
   ["run", idStr]
     | [(pId, "")] <- reads idStr ->
         case IM.lookup pId allProblems of
-          Just problem -> do
-            putStrLn $ "Running Problem #" <> show pId <> ":"
-            problemRun problem []
+          Just problem -> evalProblem problem
           Nothing -> do
             putStrLn $ "Problem #" <> show pId <> " not found."
             exitFailure
