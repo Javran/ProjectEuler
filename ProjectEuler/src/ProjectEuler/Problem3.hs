f1239c13ba8566f6b64c86b4e4a9e7232eacdc30
Javran Cheng <Javran.c@gmail.com>
2019-06-14 22:42:06 -0700

    looks better when using arithmoi

--- ProjectEuler/src/ProjectEuler/Problem3.hs
+++ ProjectEuler/src/ProjectEuler/Problem3.hs
@@ -1,7 +1,8 @@
 module ProjectEuler.Problem3 (problem) where
 
-import Data.Numbers.Primes
 import ProjectEuler.Types
+import Math.NumberTheory.Powers.Squares
+import Math.NumberTheory.Primes.Sieve
 
 problem :: Problem
 problem = pureProblem 3 Solved result
@@ -11,8 +12,9 @@ n = 600851475143
 
 result :: Integer
 result =
-  head . filter (\x -> n `mod` x == 0)
-  . reverse
-  . takeWhile (\x -> x*x <= n)
-  $ primes
-
+    head . filter (\x -> n `rem` x == 0)
+    . reverse
+    . takeWhile (<= q)
+    $ primes
+  where
+    q = integerSquareRoot' n
