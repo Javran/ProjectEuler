105c64e57984d08fc7b568dcb63778528cce36fa
Javran Cheng <Javran.c@gmail.com>
2019-06-14 23:05:27 -0700

    create problem template

---
+++ ProjectEuler/templater/mustache/ProblemX.hs
@@ -0,0 +1,10 @@
+module ProjectEuler.Problem{{{problem_id}}}
+  ( problem
+  ) where
+
+import ProjectEuler.Types
+
+problem :: Problem
+problem = pureProblem {{{problem_id}}} Unsolved result
+
+result = ()
