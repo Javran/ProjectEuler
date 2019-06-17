00181cc8ceba501abc50ac8a3fbfd56487f2c243
Javran Cheng <Javran.c@gmail.com>
2019-06-16 22:38:19 -0700

    p24: improvement & comments

--- ProjectEuler/src/ProjectEuler/Problem24.hs
+++ ProjectEuler/src/ProjectEuler/Problem24.hs
@@ -13,19 +13,39 @@ factorials = scanl (*) 1 [1..]
 factorial :: Int -> Int
 factorial = (factorials !!)
 
--- define `permutation id` x to be the identity of the (x+1)-th permutation
--- e.g. 0,1,2, pid 0 => 0,1,2, pid 1 => 0,2,1, ... etc.
--- the problem is to find the (999999+1)-th permutation for [0..9], pid = 999999
-pidToPermutation :: Int -> [Int] -> [Int]
-pidToPermutation 0 arr = arr
-pidToPermutation pid arr = h : pidToPermutation subPid t
-  where
-    n = length arr
-    subCycle = factorial $ n - 1
-    (headInd, subPid) = pid `quotRem` subCycle
-    (h,t) = breakList headInd arr
+{-
+  Given a number p in [0 .. (n!-1)], return the corresponding permutation
+  by using elements in the given list.
 
--- get the n-th element, also return the list after removing that element
+  for n = 3, xs = [0,1,2], we have:
+
+  - getPermutation 0 xs = [0,1,2]
+  - getPermutation 1 xs = [0,2,1]
+  - getPermutation 2 xs = [1,0,2]
+  - getPermutation 3 xs = [1,2,0]
+  - getPermutation 4 xs = [2,0,1]
+  - getPermutation 5 xs = [2,1,0]
+
+  note that the permutation can be broken down into 3 groups by first element:
+
+  - group 0: 0 then <permutations of {1,2}>,
+    or the result of `getPermutation p' [1,2]` with p' being relative to this group
+  - group 1: 1 then <permutations of {0,2}>
+    or the result of `getPermutation p' [0,2]` with p' being relative to this group
+  - group 2: 2 then <permutations of {0,1}>
+    or the result of `getPermutation p' [0,1]` with p' being relative to this group
+ -}
+getPermutation :: Int -> [Int] -> [Int]
+getPermutation _ xs@[_] = xs
+getPermutation p xs = h : getPermutation p' t
+  where
+    n = length xs
+    subCycle = factorial $ n - 1
+    (headInd, p') = p `quotRem` subCycle
+    (h,t) = breakList headInd xs
+
+-- remove the n-th element from the list,
+-- return the removed element and list after removal.
 breakList :: Int -> [a] -> (a, [a])
 breakList n xs = (z, ys<>zs)
   where
@@ -34,4 +54,4 @@ breakList n xs = (z, ys<>zs)
 result :: Integer
 result =
   foldl (\a i -> a*10 + fromIntegral i) 0
-  $ pidToPermutation 999999 [0..9]
+  $ getPermutation 999999 [0..9]
