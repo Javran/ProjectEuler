86f1260ec3bad9d002248683a296e8af3a01c8ff
Javran Cheng <Javran.c@gmail.com>
2019-06-17 23:18:26 -0700

    p4: done manual migration

--- ProjectEuler/src/ProjectEuler/Problem4.hs
+++ ProjectEuler/src/ProjectEuler/Problem4.hs
@@ -7,19 +7,14 @@ import ProjectEuler.Types
 problem :: Problem
 problem = pureProblem 4 Solved result
 
-result = ()
-
 isPalindromic :: (Show a, Integral a) => a -> Bool
 isPalindromic x = xs == reverse xs
   where
     xs = show x
 
-main :: IO ()
-main = print
-     . maximum
-     $ filter isPalindromic
-              [ x*y
-              | x <-[999,998..100 :: Int]
-              , y <-[x,(x-1)..100]
-              ]
+result :: Int
+result =
+  maximum
+  $ filter isPalindromic
+    [ x*y | x <- [999,998..100] , y <- [x, x-1 .. 100]]
 
