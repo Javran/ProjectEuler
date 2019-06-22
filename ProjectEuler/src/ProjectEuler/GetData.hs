e0e706fcf1753cbc3c2e25edc841e7b407952720
Javran Cheng <Javran.c@gmail.com>
2019-06-21 23:30:57 -0700

    new GetData facility ready

--- ProjectEuler/src/ProjectEuler/GetData.hs
+++ ProjectEuler/src/ProjectEuler/GetData.hs
@@ -2,14 +2,20 @@
 module ProjectEuler.GetData
   ( getDataFile
   , getDataDir
-  , rawDirContents
+  , getDataRawContent
+  , getDataContent
   ) where
 
+import Control.Arrow
 import Data.FileEmbed
+import Data.Maybe
+import Data.Text.Encoding (decodeUtf8)
 import Paths_ProjectEuler
 import System.FilePath
 
 import qualified Data.ByteString as BS
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
 import qualified System.IO.Strict as SIO
 
 -- TODO: can we use file-embed here to completely get rid of
@@ -20,5 +26,29 @@ getDataFile fName = do
     dir <- getDataDir
     SIO.readFile (dir </> "data" </> fName)
 
-rawDirContents :: [(FilePath, BS.ByteString)]
-rawDirContents = $(embedDir "data")
+{-
+  Data files are constructed at compile time to reduce runtime overhead.
+
+  Here we provides 2 versions of the same file:
+  - ByteString version, if needed for parsing.
+  - Text version, this will cover most use cases.
+ -}
+dataDirContents :: M.Map FilePath (BS.ByteString, T.Text)
+dataDirContents = M.fromList $ second (\x -> (x, decodeUtf8 x)) <$> $(embedDir "data")
+
+getDataPair :: FilePath -> (BS.ByteString, T.Text)
+getDataPair p =
+    {-
+      The error raise is intentional because `data/` path
+      is under version control and shouldn't contain any files
+      that are unknown statically.
+     -}
+    fromMaybe err $ M.lookup p dataDirContents
+  where
+    err = error $ "Data file not found: " <> p
+
+getDataRawContent :: FilePath -> BS.ByteString
+getDataRawContent = fst . getDataPair
+
+getDataContent :: FilePath -> T.Text
+getDataContent = snd . getDataPair
