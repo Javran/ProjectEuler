874dd386393da91c801a1f99e02e1161cb40ee24
Javran Cheng <Javran.c@gmail.com>
2019-06-21 23:04:06 -0700

    file-embed experiment seems working

--- ProjectEuler/src/ProjectEuler/GetData.hs
+++ ProjectEuler/src/ProjectEuler/GetData.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
 module ProjectEuler.GetData
   ( getDataFile
   , getDataDir
+  , rawDirContents
   ) where
 
+import Data.FileEmbed
 import Paths_ProjectEuler
 import System.FilePath
 
+import qualified Data.ByteString as BS
 import qualified System.IO.Strict as SIO
 
 -- TODO: can we use file-embed here to completely get rid of
@@ -15,3 +19,6 @@ getDataFile :: String -> IO String
 getDataFile fName = do
     dir <- getDataDir
     SIO.readFile (dir </> "data" </> fName)
+
+rawDirContents :: [(FilePath, BS.ByteString)]
+rawDirContents = $(embedDir "data")
