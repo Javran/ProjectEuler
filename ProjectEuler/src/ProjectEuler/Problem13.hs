da95d8f2686b221409ea844c3a500b38bfbc25fc
Javran Cheng <Javran.c@gmail.com>
2019-06-21 23:06:43 -0700

    p13 slight improvement

--- ProjectEuler/src/ProjectEuler/Problem13.hs
+++ ProjectEuler/src/ProjectEuler/Problem13.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 module ProjectEuler.Problem13
   ( problem
   ) where
@@ -10,5 +11,5 @@ problem = pureProblemWithData "p13-numbers.txt" 13 Solved compute
 compute :: String -> Integer
 compute =
   read . take 10 . show . sum
-  . map (read :: String -> Integer)  . lines
+  . map (read @Integer) . lines
 
