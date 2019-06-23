1f93566bd438a8664601ff1f2a3fed9ec2519032
Javran Cheng <Javran.c@gmail.com>
2019-06-22 19:25:00 -0700

    move parsing logic into GetData

--- ProjectEuler/src/ProjectEuler/GetData.hs
+++ ProjectEuler/src/ProjectEuler/GetData.hs
@@ -1,17 +1,34 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE
+    TemplateHaskell
+  , TypeApplications
+  , OverloadedStrings
+  #-}
 module ProjectEuler.GetData
   ( getDataRawContent
   , getDataContent
+  , expectedAnswers
+  , getExpectedAnswers
   ) where
 
+import Control.Applicative
 import Control.Arrow
+import Control.Exception
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Coerce
 import Data.FileEmbed
+import Data.Foldable
 import Data.Maybe
+import Data.Scientific
 import Data.Text.Encoding (decodeUtf8)
+import TextShow
 
 import qualified Data.ByteString as BS
+import qualified Data.HashMap.Strict as HM
+import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
+import qualified Data.Yaml as Yaml
 
 {-
   Data files are constructed at compile time to reduce runtime overhead.
@@ -39,3 +56,52 @@ getDataRawContent = fst . getDataPair
 
 getDataContent :: FilePath -> T.Text
 getDataContent = snd . getDataPair
+
+newtype Answers = Answers (IM.IntMap [T.Text]) deriving Show
+
+instance FromJSON Answers where
+  parseJSON =
+    withObject "Answers" $ \v -> do
+        obj <- v .: "answers"
+        Answers . IM.fromList <$> mapM convert (HM.toList obj)
+      where
+        convert :: (T.Text, Value) -> Parser (Int, [T.Text])
+        convert (t, xs) = do
+          [(v, "")] <- pure $ reads (T.unpack t)
+          let convertAnswerOuts :: Array -> Parser [T.Text]
+              convertAnswerOuts = mapM convertLine . toList
+                where
+                  scientificToInteger :: Scientific -> Parser Integer
+                  scientificToInteger s = do
+                    Right i <- pure (floatingOrInteger @Double s)
+                    pure i
+
+                  {-
+                    First interpret the field as Text,
+                    in case of failure, try Integer and convert it to Text.
+
+                    this allows writing:
+
+                    > - 12345
+
+                    instead of the verbose version:
+
+                    > - "12345"
+
+                   -}
+                  convertLine v' =
+                    withText "OutputLine" pure v'
+                    <|> withScientific
+                          "OutputLine"
+                          ((showt @Integer <$>) . scientificToInteger)
+                          v'
+          ys <- withArray "AnswerList" convertAnswerOuts xs
+          pure (v, ys)
+
+expectedAnswers :: IM.IntMap [T.Text]
+expectedAnswers = case Yaml.decodeEither' $ getDataRawContent "answers.yaml" of
+  Left e -> error (displayException e)
+  Right a -> coerce @Answers a
+
+getExpectedAnswers :: Int -> Maybe [T.Text]
+getExpectedAnswers pId = IM.lookup pId expectedAnswers
--- ProjectEuler/test/ProjectEuler/AllProblemsSpec.hs
+++ ProjectEuler/test/ProjectEuler/AllProblemsSpec.hs
@@ -1,76 +1,14 @@
-{-# LANGUAGE
-    OverloadedStrings
-  , TypeApplications
-  , ScopedTypeVariables
-  #-}
 module ProjectEuler.AllProblemsSpec where
 
-import Control.Applicative
-import Control.Exception
 import Control.Monad
 import Control.Monad.IO.Class
-import Data.Aeson
-import Data.Aeson.Types
-import Data.Coerce
-import Data.Foldable
-import Data.Scientific
 import Test.Hspec
-import TextShow
 
-import qualified Data.HashMap.Strict as HM
 import qualified Data.IntMap.Strict as IM
-import qualified Data.Text as T
-import qualified Data.Yaml as Yaml
 
 import ProjectEuler.AllProblems
 import ProjectEuler.GetData
 import ProjectEuler.Types
-
-newtype Answers = Answers (IM.IntMap [T.Text]) deriving Show
-
-instance FromJSON Answers where
-  parseJSON =
-    withObject "Answers" $ \v -> do
-        obj <- v .: "answers"
-        Answers . IM.fromList <$> mapM convert (HM.toList obj)
-      where
-        convert :: (T.Text, Value) -> Parser (Int, [T.Text])
-        convert (t, xs) = do
-          [(v, "")] <- pure $ reads (T.unpack t)
-          let convertAnswerOuts :: Array -> Parser [T.Text]
-              convertAnswerOuts = mapM convertLine . toList
-                where
-                  scientificToInteger :: Scientific -> Parser Integer
-                  scientificToInteger s = do
-                    Right i <- pure (floatingOrInteger @Double s)
-                    pure i
-
-                  {-
-                    First interpret the field as Text,
-                    in case of failure, try Integer and convert it to Text.
-
-                    this allows writing:
-
-                    > - 12345
-
-                    instead of the verbose version:
-
-                    > - "12345"
-
-                   -}
-                  convertLine v' =
-                    withText "OutputLine" pure v'
-                    <|> withScientific
-                          "OutputLine"
-                          ((showt @Integer <$>) . scientificToInteger)
-                          v'
-          ys <- withArray "AnswerList" convertAnswerOuts xs
-          pure (v, ys)
-
-expectedAnswers :: Answers
-expectedAnswers = case Yaml.decodeEither' $ getDataRawContent "answers.yaml" of
-  Left e -> error (displayException e)
-  Right a -> a
 
 spec :: Spec
 spec =
@@ -82,7 +20,7 @@ spec =
             Unsolved ->
               pendingWith "This problem is not yet solved."
             Solved ->
-              case IM.lookup pId . coerce $ expectedAnswers of
+              case getExpectedAnswers pId of
                 Nothing ->
                   pendingWith "Missing test case."
                 Just expectedOuts -> do
