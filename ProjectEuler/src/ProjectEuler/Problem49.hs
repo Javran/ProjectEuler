f6a43e3558e266f234262c33d79d169da896c3d0
Javran Cheng <Javran.c@gmail.com>
2019-06-25 00:27:57 -0700

    p49: cleanup

--- ProjectEuler/src/ProjectEuler/Problem49.hs
+++ ProjectEuler/src/ProjectEuler/Problem49.hs
@@ -2,13 +2,13 @@ module ProjectEuler.Problem49
   ( problem
   ) where
 
-import Data.Int
-import Math.NumberTheory.Primes
-import Data.Char
-import Data.List
-import Data.Maybe
 import Control.Monad
-import qualified Data.Map as M
+import Data.Int
+import Data.List
+import Math.NumberTheory.Primes
+
+import qualified Data.IntMap.Strict as IM
+import qualified Data.List.Ordered as LOrdered
 
 import ProjectEuler.Types
 
@@ -19,62 +19,60 @@ problem = pureProblem 49 Solved result
 -- step 2: group by permutation closure
 -- step 3: find arithmetic seq
 
-limitedPrimes = takeWhile (<10000) $ dropWhile (<1000) primes
+intToDigits :: Int -> [Int]
+intToDigits x = ($ []) . foldr (.) id $ unfoldr f x
+  where
+    f 0 = Nothing
+    f n = let (q,r) = n `quotRem` 10 in Just ((++[r]), q)
 
-numToDigitList num = map (\ch -> ord ch - ord '0') $ show num
+digitsToInt :: [Int] -> Int
+digitsToInt = foldl (\a b -> a*10+b) 0
 
-digitListToNum = foldl (\acc i -> acc * 10 + i) 0
-
--- remove first occurrence of x from ls
-removeFirst x ls = before ++ after
-    where
-        before = takeWhile (/=x) ls
-        after = tail $ dropWhile (/=x) ls
-
-newtype Permu = Permu [Int]
-    deriving Show
-
-instance Eq Permu where
-    (Permu x) == (Permu y) = sort x == sort y
-
-instance Ord Permu where
-    (Permu x) `compare` (Permu y) = sort x `compare` sort y
-
-updateMap m p = newMap
-    where
-        result = M.lookup p m
-        (Permu xs) = p
-        newMap = if isNothing result
-            then M.insert p [xs] m
-            else M.update (Just . (xs:)) p m
-
--- given that a1 and a2 are in the list, grow to form
---   the longest seq using elements from arr
-growPair arr a1 a2 =
-    if a3 `elem` arr
-        then a1: growPair arr a2 a3
-        else [a1,a2]
-    where a3 = a2 + (a2 - a1)
+limitedPrimes :: [Int]
+limitedPrimes = takeWhile (< 10000) $ dropWhile (< 1000) primes
 
 -- assume arithmetic sequence should at least have 2 elements
+arithSeq :: [Int] -> [[Int]]
 arithSeq arr = do
     let n = length arr
-    i <- [  0..n-1]
+    i <- [0..n-1]
     j <- [i+1..n-1]
-    return $ growPair arr (arr !! i) (arr !! j)
+    pure $ growPair (arr !! i) (arr !! j)
+  where
+    -- given that a1 and a2 are in the list, grow to form
+    --   the longest seq using elements from arr
+    growPair :: Int -> Int -> [Int]
+    growPair a1 a2 =
+      if a3 `elem` arr
+        then a1 : growPair a2 a3
+        else [a1,a2]
+      where
+        a3 = a2 + (a2 - a1)
 
 -- only interested in long seq
-longArithSeq arr = filter ((>2).length) $ arithSeq arr
+longArithSeq :: [Int] -> [[Int]]
+longArithSeq = foldMap f . arithSeq
+  where
+    -- at least 3 elements
+    f xs@(_:_:_:_rest) = [xs]
+    f _ = []
 
 result :: Int64
-result = read $ show x <> show y <> show z
+result = read $ foldMap show ans
   where
-    permuPrimeList = map (Permu . numToDigitList) limitedPrimes
-    m = map snd $ M.toList $ foldl updateMap M.empty permuPrimeList
-    -- for each group, we want every elements in it converted back to a num
-    --   we only need closures with size >= 3
-    permClosures = map sort $ filter ((>= 3).length) $ map (liftM digitListToNum) m
-
-
-    [[[x,y,z]]] = filter (\ xs -> (not . null) xs && not (any (1487 `elem`) xs))
-      $ map longArithSeq permClosures
+    -- group together prime number that are permutations of each other.
+    -- elements of this list is sorted (by using LOrdered.union).
+    primeGroups :: [[Int]]
+    primeGroups = IM.elems (IM.fromListWith LOrdered.union (toPair <$> limitedPrimes))
+      where
+        toPair x = (digitsToInt . sort . intToDigits $ x, [x])
+    [[ans]] = do
+      -- for each group, we want every elements in it converted back to a num
+      --   we only need closures with size >= 3
+      pg@(_:_:_:_:_rest) <- primeGroups
+      -- xs is not null
+      xs@(_:_) <- [longArithSeq pg]
+      -- since the list is sorted, we just want to check the first
+      -- element of the group to exclude the known one.
+      guard $ (/= 1487) . head . head $ xs
+      pure xs
