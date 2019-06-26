77435609f72a5359dd87d6187d0425609a557bc7
Javran Cheng <Javran.c@gmail.com>
2019-06-25 23:59:52 -0700

    p53: note about avoiding overflow

--- ProjectEuler/src/ProjectEuler/Problem53.hs
+++ ProjectEuler/src/ProjectEuler/Problem53.hs
@@ -10,6 +10,14 @@ problem = pureProblem 53 Solved result
 sel :: Integer -> Integer -> Integer
 sel n r = product [n-r+1..n] `div` product [1..r]
 
+-- NOTE: it's tempting to do the following
+-- so that we can do
+-- > 1 {* (n-r+1) / 1} {* (n-r+2) / 2} {* (n-r+3) / 3} ...
+-- to minimize overflow Int ... but this doesn't help as the final result will
+-- still blow up.
+_sel :: Integer -> Integer -> Integer
+_sel n r = foldl (\acc (x,y) -> acc * x `quot` y) 1 $ zip [n-r+1..n] [1..r]
+
 result :: Int
 result = length [ () | n <- [1..100], r <- [0..n], sel n r > 1000000 ]
 
