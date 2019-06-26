2f18c0ef932024439fd32dc9aa7d374ad2b3446a
Javran Cheng <Javran.c@gmail.com>
2019-06-25 23:49:28 -0700

    p53: cleanup

--- ProjectEuler/src/ProjectEuler/Problem53.hs
+++ ProjectEuler/src/ProjectEuler/Problem53.hs
@@ -10,6 +10,6 @@ problem = pureProblem 53 Solved result
 sel :: Integer -> Integer -> Integer
 sel n r = product [n-r+1..n] `div` product [1..r]
 
-result :: Integer
-result = sum [1 | n <- [1..100], r<-[0..n], sel n r >1000000 ]
+result :: Int
+result = length [ () | n <- [1..100], r <- [0..n], sel n r > 1000000 ]
 
