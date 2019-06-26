802017086a4ff989b92a7b5d1669f3aeb111d8eb
Javran Cheng <Javran.c@gmail.com>
2019-06-25 20:12:12 -0700

    p51: optimize

--- ProjectEuler/src/ProjectEuler/Problem51.hs
+++ ProjectEuler/src/ProjectEuler/Problem51.hs
@@ -4,27 +4,23 @@ module ProjectEuler.Problem51
 
 import Control.Monad
 import Math.NumberTheory.Primes
-import Data.Char
 
 import ProjectEuler.Types
+import ProjectEuler.Problem35 (intToDigits, digitsToInt)
 
 problem :: Problem
 problem = pureProblem 51 Solved result
 
--- generate a list that every element contains `1`
---   to save some conversion time, just keep it as Strings
-contain1Nums :: [String]
-contain1Nums = filter contains1 $ map show [0..]
-    where
-        contains1 = elem '1'
+type Digits = [Int]
 
--- search space, we are finding primes,
---   so the last digit must be one of:
---   [1,3,7,9]
-searchSpace :: [String]
-searchSpace = filter possiblePrime contain1Nums
-    where
-        possiblePrime = (`elem` "1379") . last
+-- search space, we are looking for
+-- prime numbers that contains '1'.
+searchSpace :: [Digits]
+searchSpace = do
+  n <- primes
+  let ds = intToDigits n
+  guard (1 `elem` ds)
+  pure ds
 
 -- from a number that contains 1 to all posible masks
 --   here is `1` plays the role of indicating that
@@ -32,54 +28,57 @@ searchSpace = filter possiblePrime contain1Nums
 --   a mask is a list of positions
 -- e.g.: 3121
 -- => [[1,3],[1],[3]]
-oneNumToMask :: String -> [[Int]]
-oneNumToMask xs = filter (not . null) $ powerset onePos
-    where
-        -- all position of ones
-        --   all possible masks is just the non-empty powerset of this
-        onePos = map fst $ filter ((== '1'). snd) $ zip [0..] xs
+oneNumToMask :: Digits -> [[Int]]
+oneNumToMask xs =
+    -- first element is empty therefore ignore it.
+    tail $ powerset onePos
+  where
+    -- all position of ones
+    --   all possible masks is just the non-empty powerset of this
+    onePos = map fst $ filter ((== 1). snd) $ zip [0..] xs
 
--- a trick to return the powerset of a list
+-- compute the powerset of a list
 powerset :: [a] -> [[a]]
-powerset = filterM (const [True, False])
-
-answers :: [([Int],[Int])]
-answers = do
-    -- num that contains 1
-    nStr <- searchSpace
-    -- pick up one of the possible mask
-    mask <- oneNumToMask nStr
-    -- apply the mask to obtain the prime number family
-    let family = filter (isPrime . fromIntegral) $ applyMask nStr mask
-    guard $ length family == 8
-    return (family,mask)
+powerset = filterM (const [False, True])
 
 -- apply the mask, get the family of 10 numbers
-applyMask nStr mask = map doAll transRange
-    where
-              -- 3. convert them back into integers
-        doAll = read
-              -- 2. apply transformers so we get the family in their string form
-              . (\t -> t indexedNStr)
-              -- 1. make transformers from numbers
-              . transformTo
+applyMask :: Digits -> [Int] -> [Int]
+applyMask digits mask = doAll <$> transRange
+  where
+    doAll :: Int -> Int
+    doAll =
+      -- 3. convert them back into integers
+      digitsToInt
+      -- 2. apply transformers so we get the family in their string form
+      . ($ indexedDigits)
+      -- 1. make transformers from numbers
+      . transformTo
 
-        indexedNStr = zip [0..] nStr
-        transRange = if 0 `elem` mask
-                        then [1..9]
-                        else [0..9]
-        -- transformTo takes a number `n`, and then an `indexedNStr`
-        --   changes masked digits to `n` according to the setting of `mask`
-        --   e.g.:
-        --   zip [0..] "12311"
-        --   transformTo 9 (zip [0..] "12311"),
-        --                             ^  ^
-        --                     mask = [0__3_]
-        --   => "92391"
-        transformTo :: Int -> [(Int,Char)] -> String
-        transformTo n = map (\x ->
-                if fst x `elem` mask
-                    then chr $ n + ord '0'
-                    else snd x)
+    indexedDigits = zip [0..] digits
+    transRange = if 0 `elem` mask then [1..9] else [0..9]
 
-result = head . fst . head $ answers
+    -- transformTo takes a number `n`, and then an `indexedNStr`
+    --   changes masked digits to `n` according to the setting of `mask`
+    --   e.g.:
+    --   zip [0..] "12311"
+    --   transformTo 9 (zip [0..] "12311"),
+    --                             ^  ^
+    --                     mask = [0__3_]
+    --   => "92391"
+    transformTo :: Int -> [(Int,Int)] -> [Int]
+    transformTo n = map (\(i,v) -> if i `elem` mask then n else v)
+
+result :: Int
+result = head $ head answers
+  where
+    answers = do
+      -- num that contains 1
+      nDigits <- searchSpace
+      -- pick up one of the possible mask
+      mask <- oneNumToMask nDigits
+      -- apply the mask to obtain the prime number family
+      -- the pattern match takes the advantage of List being a MonadFail
+      -- to only allow list of exactly 8 elements.
+      family@[_1,_2,_3,_4,_5,_6,_7,_8] <-
+        [filter (isPrime . fromIntegral) $ applyMask nDigits mask]
+      pure family
