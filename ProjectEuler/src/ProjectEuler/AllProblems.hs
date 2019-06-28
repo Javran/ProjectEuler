284a311adc8f8c9c92679fa6fd5c2f75485c07cb
Javran Cheng <Javran.c@gmail.com>
2019-06-27 20:02:32 -0700

    p78: migrate

--- ProjectEuler/src/ProjectEuler/AllProblems.hs
+++ ProjectEuler/src/ProjectEuler/AllProblems.hs
@@ -85,6 +85,7 @@ import ProjectEuler.Problem74
 import ProjectEuler.Problem75
 import ProjectEuler.Problem76
 import ProjectEuler.Problem77
+import ProjectEuler.Problem78
 import ProjectEuler.Problem84
 import ProjectEuler.Problem87
 import ProjectEuler.Problem88
@@ -172,6 +173,7 @@ allProblems =
     , (75, ProjectEuler.Problem75.problem)
     , (76, ProjectEuler.Problem76.problem)
     , (77, ProjectEuler.Problem77.problem)
+    , (78, ProjectEuler.Problem78.problem)
     , (84, ProjectEuler.Problem84.problem)
     , (87, ProjectEuler.Problem87.problem)
     , (88, ProjectEuler.Problem88.problem)
---
+++ ProjectEuler/src/ProjectEuler/Problem78.hs
@@ -0,0 +1,53 @@
+module ProjectEuler.Problem78
+  ( problem
+  ) where
+
+import Petbox
+
+import qualified Data.Array.Unboxed as A
+import qualified Data.Array.ST as A
+
+import ProjectEuler.Types
+
+problem :: Problem
+problem = pureProblem 78 Solved result
+
+interleave :: [a] -> [a] -> [a]
+interleave [] ys = ys
+interleave (x:xs) ys = x : interleave ys xs
+
+gpNumbers :: Integral a => [a]
+gpNumbers = [ (k*(3*k-1)) `div` 2 | k <- interleave [1..] [-1,-2..] ]
+
+trim :: Int -> Int
+trim = (`mod` 1000000)
+
+pFast :: Int -> Int
+pFast n
+    | n < 0 = 0
+    | n == 0 = 1
+    | otherwise = table A.! n
+  where
+    table = A.runSTUArray $ do
+        -- kind of cheating, the value is first found
+        -- and after that we write a faster version
+        -- knowing the upbound...
+        let limit = 60000
+        mary <- A.newArray (0,limit) 0
+        A.writeArray mary 0 1
+        let writeVal k = do
+                let gps = takeWhile (<= k) gpNumbers
+                vs <- mapM (\x -> A.readArray mary (k - x)) gps
+                let r = trim . sum . zipWith (*) (cycle [1,1,-1,-1]) $ vs
+                A.writeArray mary k r
+                pure result
+        mapM_ writeVal [1..limit]
+        pure mary
+
+result :: Int
+result =
+  fst . firstSuchThat ((== 0) . (`mod` 1000000) . snd)
+  . map (keepInput pFast)
+  $ [1..]
+  -- $ evalState (mapM (keepInputM p) [1..]) IM.empty
+
