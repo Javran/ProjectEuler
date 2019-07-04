10bd98365cc0812169aa3a8394fab8f92f0e4398
Javran Cheng <Javran.c@gmail.com>
2019-07-03 23:53:51 -0700

    get the approximation right

--- src/ProjectEuler/Problem66.hs
+++ src/ProjectEuler/Problem66.hs
@@ -2,15 +2,23 @@ module ProjectEuler.Problem66
   ( problem
   ) where
 
+import Data.Ratio
+
 import ProjectEuler.Types
-import ProjectEuler.Problem64 (gen, getPeriod)
+import ProjectEuler.Problem64 (gen)
 
 problem :: Problem
 problem = pureProblem 66 Unsolved result
+
+approx :: [Integer] -> [Rational -> Rational]
+approx = tail . scanl (\acc i t -> acc (1 / (i % 1 + t))) id
 
 {-
   Some leads:
   - https://en.wikipedia.org/wiki/Pell's_equation
   - Problem64
  -}
-result = (\z -> (z, take 10 . gen $ z)) <$> [2,3,5,6,7]
+result :: [Double]
+result =
+  take 10 . map (fromRational . (1 /) . ($ 0))
+  . approx . map (toInteger . fst) $ gen 7
