075694fc9f4e33dd4202988af8f197e92f064967
Javran Cheng <Javran.c@gmail.com>
2019-07-04 23:52:55 -0700

    p95: explore

--- src/ProjectEuler/Problem95.hs
+++ src/ProjectEuler/Problem95.hs
@@ -4,11 +4,12 @@ module ProjectEuler.Problem95
 
 import Math.NumberTheory.Primes
 import Math.NumberTheory.ArithmeticFunctions
-
+import Control.Monad.State
 import Data.Maybe
 
 import qualified Data.List.Ordered as LOrdered
 import qualified Data.IntSet as IS
+import qualified Data.IntMap as IM
 
 import ProjectEuler.Types
 
@@ -49,12 +50,26 @@ aliquotSet n = do
   s <- aliquotSet' IS.empty n
   pure . sum $ IS.toList s
 
-next :: Int -> Maybe Int
+sumOfProperDivisors :: Int -> State (IM.IntMap Int) Int
+sumOfProperDivisors n
+  | n <= 1 = pure 0
+  | isPrime (fromIntegral n) = pure 1
+  | otherwise = do
+      z <- gets (IM.lookup n)
+      case z of
+        Nothing -> do
+          let v = (sum . IS.toList $ divisorsSmall n) - n
+          modify (IM.insert n v)
+          pure v
+        Just v -> pure v
+
+next :: Int -> Maybe (Int, Int)
 next 0 = Nothing
 next 1 = Nothing
-next n
-  | isPrime (fromIntegral n) = Nothing
-  | otherwise = Just $ (sum . IS.toList $ divisorsSmall n) - n
+next n = if vs == [1] then Nothing else Just (n, x)
+  where
+    vs = init . IS.toList $ divisorsSmall n
+    x = sum vs
 
 aliquotSet' :: IS.IntSet -> Int -> Maybe IS.IntSet
 aliquotSet' s 1 = Nothing
@@ -83,8 +98,22 @@ mergeIntoGroups (x:xs) s =
     then x : mergeIntoGroups xs s
     else IS.union x s : xs
 
-result = length $
+{-
+result2 = length $
   iterate
     (IS.toList . IS.filter (not . isPrime . fromIntegral) . IS.fromList . mapMaybe next)
     initSearchSpace
   !! 2
+-}
+-- looks like memoization is faster.
+{-result = (`evalState` IM.empty) $ do
+  ns <- mapM sumOfProperDivisors initSearchSpace
+  pure (sum ns)
+-}
+
+-- just want to see how many that appears both as input and output
+result = IS.size (IS.intersection srcs dsts)
+  where
+    srcs = IS.fromDistinctAscList $ map fst pairs
+    dsts = IS.fromList $ map snd pairs
+    pairs = mapMaybe next initSearchSpace
