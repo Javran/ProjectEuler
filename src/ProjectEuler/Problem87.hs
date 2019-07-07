f91bfd28b24daec85228473356484a26a7cb8393
Javran Cheng <Javran.c@gmail.com>
2019-07-06 22:48:29 -0700

    p87: a bit cleanup

--- src/ProjectEuler/Problem87.hs
+++ src/ProjectEuler/Problem87.hs
@@ -2,7 +2,6 @@ module ProjectEuler.Problem87
   ( problem
   ) where
 
-
 import Petbox
 import qualified Data.IntSet as IS
 
@@ -11,20 +10,20 @@ import ProjectEuler.Types
 problem :: Problem
 problem = pureProblem 87 Solved result
 
+primesP2, primesP3, primesP4 :: [Int]
+primesP2 = (^! 2) <$> primes
+primesP3 = (^! 3) <$> primes
+primesP4 = (^! 4) <$> primes
+
 searchSpace :: [Int]
 searchSpace = do
-    pFt <-
-      takeWhile leLimit
-      $ (^! 4) <$> primes
-    pCb <-
-      takeWhile (leLimit . (+ pFt))
-      $ (^! 3) <$> primes
-    pSq <-
-      takeWhile (leLimit . (+ (pFt+pCb)))
-      $ (^! 2) <$> primes
-    pure (pSq + pCb + pFt)
+    pFt <- takeWhile (<= limit) primesP4
+    pCb <- takeWhile (<= limit - pFt) primesP3
+    let tmp = pFt + pCb
+    pSq <- takeWhile (<= limit - tmp) primesP2
+    pure (pSq + tmp)
   where
-    leLimit = (<= 50000000)
+    limit = 50000000
 
 result :: Int
 result = IS.size $ IS.fromList searchSpace
