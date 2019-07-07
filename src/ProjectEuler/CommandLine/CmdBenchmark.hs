84dcf588ed5500de1a2b4bbae43a2dace7e34226
Javran Cheng <Javran.c@gmail.com>
2019-07-06 23:53:50 -0700

    we need other ways of providing benchmark.

--- src/ProjectEuler/CommandLine/CmdBenchmark.hs
+++ src/ProjectEuler/CommandLine/CmdBenchmark.hs
@@ -2,47 +2,13 @@ module ProjectEuler.CommandLine.CmdBenchmark
   ( cmdBenchmark
   ) where
 
-import qualified Data.IntMap.Strict as IM
-
-import Control.Exception
-import Criterion.Main
-import System.Environment
-import System.Exit
-
-import qualified Data.Text as T
-
-import ProjectEuler.GetData
-import ProjectEuler.AllProblems
-
-import ProjectEuler.CommandLine.Common
-
 {-
-  This sub-command wraps around criterion to provide benchmark features.
+  TODO: GHC is being too smart here.
+  We'll need a script that runs the binary several times
+  to get an average, rather than having GHC figure it out
+  that it can share the result (which is exactly what
+  we don't want for benchmarking)
  -}
 
 cmdBenchmark :: [String] -> IO ()
-cmdBenchmark args
-  | idStr:benchArgs <- args
-  , [(pId, "")] <- reads idStr =
-    case IM.lookup pId allProblems of
-      Just problem -> do
-        let solRun :: IO (Double, [T.Text])
-            solRun = do
-              (t, r) <- runProblem problem
-              case r of
-                Left e -> throw e
-                Right outs ->
-                  case getExpectedAnswers pId of
-                    Nothing -> pure (t,outs)
-                    Just expectedOuts ->
-                      if expectedOuts == outs
-                        then pure (t,outs)
-                        else error "Unexpected output."
-        withArgs benchArgs $
-          defaultMain
-            [bench ("Problem #" <> show pId) $ nfIO solRun]
-      Nothing -> do
-        putStrLn $ "Problem #" <> show pId <> " not found."
-        exitFailure
-  | otherwise =
-      putStrLn "pet benchmark <problem id> [...benchmark args...]"
+cmdBenchmark _ = pure ()
