4729a36f493fbb6c90e7750dd927a52750f91260
Javran Cheng <Javran.c@gmail.com>
2019-07-07 00:35:13 -0700

    impl benchmark with correct measurement.

--- src/ProjectEuler/CommandLine/CmdBenchmark.hs
+++ src/ProjectEuler/CommandLine/CmdBenchmark.hs
@@ -1,14 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
 module ProjectEuler.CommandLine.CmdBenchmark
   ( cmdBenchmark
   ) where
 
+import Criterion.Main
+import System.CPUTime
+import System.Environment
+import System.Exit
+import TextShow
+import Turtle.Prelude
+
+import qualified Data.IntMap.Strict as IM
+
+import ProjectEuler.AllProblems
+import ProjectEuler.CommandLine.Common
+
 {-
-  TODO: GHC is being too smart here.
-  We'll need a script that runs the binary several times
-  to get an average, rather than having GHC figure it out
-  that it can share the result (which is exactly what
-  we don't want for benchmarking)
+  This sub-command wraps around criterion to provide benchmark features.
  -}
 
+{-
+
+  Note: we benchmark the program by running solution as an external binary.
+  By doing so we make sure GHC doesn't being too smart and just reuse results
+  among execution of the same IO action.
+
+ -}
+runProblemAsExternalBinary :: Int -> IO Double
+runProblemAsExternalBinary pId = do
+  tStart <- getCPUTime
+  (ExitSuccess, _) <- procStrict "./pet" ["run", showt pId] ""
+  tEnd <- getCPUTime
+  pure $ fromIntegral (tEnd - tStart) / (10^(9 :: Int))
+
 cmdBenchmark :: [String] -> IO ()
-cmdBenchmark _ = pure ()
+cmdBenchmark args
+  | idStr:benchArgs <- args
+  , [(pId, "")] <- reads idStr =
+    case IM.lookup pId allProblems of
+      Just _ -> do
+        prjHome <- getProjectHome
+        cd prjHome
+        withArgs benchArgs $
+          defaultMain
+            [bench ("Problem #" <> show pId) $ nfAppIO runProblemAsExternalBinary pId]
+      Nothing -> do
+        putStrLn $ "Problem #" <> show pId <> " not found."
+        exitFailure
+  | otherwise =
+      putStrLn "pet benchmark <problem id> [...benchmark args...]"
