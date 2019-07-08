a57952d3a3f2c2fe65b07d33e91a6dcd27aade55
Javran Cheng <Javran.c@gmail.com>
2019-07-07 22:49:28 -0700

    p95: cleanup, compute in one go.

--- src/ProjectEuler/Problem95.hs
+++ src/ProjectEuler/Problem95.hs
@@ -40,80 +40,19 @@ problem = pureProblem 95 Unsolved result
 maxN :: Int
 maxN = 1000000
 
--- initSearchSpace :: IS.IntSet
+initSearchSpace :: [Int]
 initSearchSpace = [1..maxN] `LOrdered.minus` primes
 
-aliquotSet :: Int -> Maybe Int
-aliquotSet 0 = Nothing
-aliquotSet 1 = Nothing
-aliquotSet n = do
-  s <- aliquotSet' IS.empty n
-  pure . sum $ IS.toList s
-
-sumOfProperDivisors :: Int -> State (IM.IntMap Int) Int
+sumOfProperDivisors :: Int -> Int
 sumOfProperDivisors n
-  | n <= 1 = pure 0
-  | isPrime (fromIntegral n) = pure 1
-  | otherwise = do
-      z <- gets (IM.lookup n)
-      case z of
-        Nothing -> do
-          let v = (sum . IS.toList $ divisorsSmall n) - n
-          modify (IM.insert n v)
-          pure v
-        Just v -> pure v
+  | n <= 1 = 0
+  | otherwise =
+      IS.foldl' (+) 0 (divisorsSmall n) - n
 
-next :: Int -> Maybe (Int, Int)
-next 0 = Nothing
-next 1 = Nothing
-next n = if vs == [1] then Nothing else Just (n, x)
-  where
-    vs = init . IS.toList $ divisorsSmall n
-    x = sum vs
-
-aliquotSet' :: IS.IntSet -> Int -> Maybe IS.IntSet
-aliquotSet' s 1 = Nothing
-aliquotSet' s n
-  | isPrime (fromIntegral n) = Nothing
-  | IS.member n s = Just s
-  | n > maxN = Nothing
-  | otherwise = aliquotSet' (IS.insert n s) next
-  where
-    next = (sum . IS.toList $ divisorsSmall n) - n
-
-{-
-search :: IS.IntSet -> [IS.IntSet] -> [IS.IntSet]
-search searchSpace groups = case IS.maxView searchSpace of
-  Nothing -> groups
-  Just (x, _) ->
-    let aliquotX = aliquotSet x
-        searchSpace' = searchSpace `IS.difference` aliquotX
-    in search searchSpace' (mergeIntoGroups groups aliquotX)
- -}
-
-mergeIntoGroups :: [IS.IntSet] -> IS.IntSet -> [IS.IntSet]
-mergeIntoGroups [] s = [s]
-mergeIntoGroups (x:xs) s =
-  if IS.null (IS.intersection x s)
-    then x : mergeIntoGroups xs s
-    else IS.union x s : xs
-
-{-
-result2 = length $
-  iterate
-    (IS.toList . IS.filter (not . isPrime . fromIntegral) . IS.fromList . mapMaybe next)
-    initSearchSpace
-  !! 2
--}
--- looks like memoization is faster.
-{-result = (`evalState` IM.empty) $ do
-  ns <- mapM sumOfProperDivisors initSearchSpace
-  pure (sum ns)
--}
-
--- just want to see how many that appears both as input and output
-result = IS.size (IS.intersection srcs dsts)
+result = IS.size srcs
   where
     srcs = IS.fromDistinctAscList $ map fst pairs
-    dsts = IS.fromList $ map snd pairs
-    pairs = mapMaybe next initSearchSpace
+    pairs =
+      concatMap
+        (\n -> let s = sumOfProperDivisors n in if s <= 1 then [] else [(n,s)])
+        initSearchSpace
