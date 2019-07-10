05336769bb84793b7ece68d8c41908c5a97bdc11
Javran Cheng <Javran.c@gmail.com>
2019-07-09 22:50:22 -0700

    p97: a bit more aggressive optimization.

--- src/ProjectEuler/Problem97.hs
+++ src/ProjectEuler/Problem97.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE TypeApplications #-}
 module ProjectEuler.Problem97
   ( problem
   ) where
 
 import Data.Int
 import Data.Function
+
+import Math.NumberTheory.Powers.Squares
+
 import ProjectEuler.Types
 
 problem :: Problem
@@ -12,6 +16,9 @@ problem = pureProblem 97 Solved result
 base :: Integral i => i
 base = 10000000000
 
+lim :: Int64
+lim = fromInteger $ integerSquareRoot' (fromIntegral @Int64 @Integer maxBound)
+
 {-
   2^64 has 20 digits in decimal, so if we are multiplying
   two numbers that both have 10 digits, overflow could happen,
@@ -19,7 +26,16 @@ base = 10000000000
   so that overall we are only passing Int64 between functions.
  -}
 mul :: Int64 -> Int64 -> Int64
-mul x y = fromInteger $ ((*) `on` fromIntegral) x y `mod` base
+mul x y
+  | x <= lim && y <= lim =
+    {-
+      If both x and y are within representation range of Int64,
+      there's no risk of overflow, this avoids the expensive
+      mulplication for Integer type.
+     -}
+    (x * y) `mod` base
+  | otherwise =
+    fromInteger $ ((*) `on` fromIntegral) x y `mod` base
 
 {-
   the following function is ported from
