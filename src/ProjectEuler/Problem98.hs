8b4762aee972e6519164f5a8a8e5d9e04f77514c
Javran Cheng <Javran.c@gmail.com>
2019-07-10 22:30:47 -0700

    p98: some analysis on the topic

--- src/ProjectEuler/Problem98.hs
+++ src/ProjectEuler/Problem98.hs
@@ -3,6 +3,7 @@ module ProjectEuler.Problem98
   ) where
 
 import Data.List
+import Data.Ord
 import qualified Data.Text as T
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
@@ -30,5 +31,26 @@ groupAnagrams =
     mkGroup [] = []
     mkGroup [_] = []
     mkGroup xs@(h:_) = [(S.fromList h, xs)]
+
+{-
+  Now that we know in the worst case we'll have 9 characters
+  that we need to assign each of them a value.
+
+  so search space for trying all possible assignment will be:
+
+  > product $ take 9 [9,8..]
+  362880
+
+  Of course first letter cannot be assigned 0, so this is an overestimation.
+
+  Also there are only 42 groups after processing through "groupAnagrams",
+  with many of them have only 4~6 letters to be assigned,
+  I imagine brute forcing this won't take long.
+
+  Also as a pontential optimization, we can choose to ignore Char
+  and instead just work with sequence of numbers, but we are not going to do that
+  until we have a working solution.
+
+ -}
 
 compute = show . groupAnagrams . parseWords
