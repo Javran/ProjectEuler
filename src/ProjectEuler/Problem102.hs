7d677a6ae92cf71d880350eb3b08f5873782dd24
Javran Cheng <Javran.c@gmail.com>
2019-07-14 22:59:23 -0700

    p102: setup for parsing input.

--- src/ProjectEuler/Problem102.hs
+++ src/ProjectEuler/Problem102.hs
@@ -2,11 +2,33 @@ module ProjectEuler.Problem102
   ( problem
   ) where
 
-import ProjectEuler.Types
+import Data.Monoid
+
+import qualified Data.Text as T
+
+import ProjectEuler.GetData
+
 
 problem :: Problem
-problem = pureProblem 102 Unsolved result
+problem = pureProblemWithData "p102_triangles.txt" 102 Unsolved compute
 
-result = ()
+type Coord = (Int, Int)
 
+parseCoords :: String -> [Coord]
+parseCoords raw = [(a,b),(c,d),(e,f)]
+  where
+    [a,b,c,d,e,f] = read ("[" <> raw <> "]")
 
+containsTheOrigin :: [Coord] -> Sum Int
+containsTheOrigin [_a,_b,_c] = 0 -- TODO
+containsTheOrigin _ = error "unexpected input"
+
+compute :: T.Text -> Int
+compute raw =
+    getSum $ foldMap containsTheOrigin triangles
+  where
+    triangles =
+      fmap parseCoords
+      . lines
+      . T.unpack
+      $ raw
