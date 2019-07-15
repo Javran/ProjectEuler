742c6db74274d0b3fb25b1e95995551ecb711c94
Javran Cheng <Javran.c@gmail.com>
2019-07-14 23:11:39 -0700

    p102: should be solution.

--- src/ProjectEuler/Problem102.hs
+++ src/ProjectEuler/Problem102.hs
@@ -13,14 +13,41 @@ problem :: Problem
 problem = pureProblemWithData "p102_triangles.txt" 102 Unsolved compute
 
 type Coord = (Int, Int)
+type Vec = (Int, Int)
 
 parseCoords :: String -> [Coord]
 parseCoords raw = [(a,b),(c,d),(e,f)]
   where
     [a,b,c,d,e,f] = read ("[" <> raw <> "]")
 
+cDiff :: Coord -> Coord -> Vec
+cDiff (x,y) (u,v) = (x-u,y-v)
+
+cross :: Vec -> Vec -> Int
+cross (a0,a1) (b0,b1) = a0*b1 - a1*b0
+
 containsTheOrigin :: [Coord] -> Sum Int
-containsTheOrigin [_a,_b,_c] = 0 -- TODO
+containsTheOrigin [ca,cb,cc] =
+    if all (> 0) allNonZeroCross || all (< 0) allNonZeroCross
+      then 1
+      else 0
+  where
+    o = (0,0)
+
+    vAB = cDiff cb ca
+    vBC = cDiff cc cb
+    vCA = cDiff ca cc
+
+    vAO = cDiff o ca
+    vBO = cDiff o cb
+    vCO = cDiff o cc
+
+    crossA = cross vAB vAO
+    crossB = cross vBC vBO
+    crossC = cross vCA vCO
+
+    allNonZeroCross = filter (/= 0) [crossA, crossB, crossC]
+
 containsTheOrigin _ = error "unexpected input"
 
 compute :: T.Text -> Int
