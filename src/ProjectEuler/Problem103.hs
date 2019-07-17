a1526156119e52da8938e83d3f7e7449a4d2467b
Javran Cheng <Javran.c@gmail.com>
2019-07-16 22:01:22 -0700

    p103 & p105: cleanup & backport

--- src/ProjectEuler/Problem103.hs
+++ src/ProjectEuler/Problem103.hs
@@ -2,12 +2,8 @@ module ProjectEuler.Problem103
   ( problem
   ) where
 
-import Control.Monad
-import Data.List
-import Data.Bits
-import qualified Data.IntSet as IS
-
 import ProjectEuler.Types
+import ProjectEuler.Problem105 (findSpecialSubsets)
 
 problem :: Problem
 problem = pureProblem 103 Solved result
@@ -24,63 +20,6 @@ problem = pureProblem 103 Solved result
  -}
 
 {-
-  like "pick", but whenever an element picked,
-  all elements before it will be dropped. This has the effect of only picking
-  elements in order.
- -}
-pickInOrder :: [a] -> [] (a,[a])
-pickInOrder = fmap (\(x:xs) -> (x,xs)) . init . tails
-
-isIncreasing :: Ord a => [a] -> Bool
-isIncreasing xs = and $ zipWith (<) xs (tail xs)
-
-{-
-  - candidates: list of not yet used candidates
-  - sumSets: list of sum set:
-    + sumSets !! 0 => always a singleton set {0}
-    + sumSets !! 1 => set of sum of subsets of size 1
-    + sumSets !! 2 => set of sum of subsets of size 2
-    + etc.
- -}
-solve :: [Int] -> Int -> [Int] -> [IS.IntSet] -> [[Int]]
-solve curList sz candidates sumSets
-  | sz == 7 = pure (reverse curList)
-  | otherwise = do
-      (x,candidates') <- pickInOrder candidates
-      let sumSetsWithX =
-            -- since we know this map is strictly monotonic,
-            -- we can bypass checks by reconstructing altogether.
-            IS.fromDistinctAscList . fmap (+x) . IS.toAscList <$> sumSets
-          sumSets' = zipWith IS.union (sumSets <> [IS.empty]) (IS.empty : sumSetsWithX)
-          allSums' = foldl' IS.union IS.empty sumSets'
-          getMinMax s
-            | IS.null s = []
-            | IS.size s == 1 = [IS.findMin s]
-            | otherwise = [IS.findMin s, IS.findMax s]
-
-      -- a note about a simple trick:
-      -- most of the time it's faster to write down guards
-      -- separately rather than combining them with `&&`.
-      -- I'm not sure why this is the case though,
-      -- for now my theory is that `&&`  does has some extra cost
-      -- of doing pattern matching before return value to `guard`,
-      -- but I'm not convinced by my own theory and believe ghc should
-      -- take care of nested pattern matching (one in `&&` and one in `guard`).
-
-      -- to make sure that all sums are unique, we can simply verify that
-      -- the # of elements in list is expected.
-      guard $ IS.size allSums' == 1 `unsafeShiftL` (sz+1)
-      -- for the size vs sum condition to hold, we just want to know
-      -- if we were to extra min and max from each element of sumSets'
-      -- (of course for singleton sets we only need one element)
-      -- do we end up with an increasing sequence
-      -- or, in other words, this is the same as asking that
-      -- the range of each pair of neighborhood elements in allSums'
-      -- shouldn't overlap.
-      guard $ isIncreasing (foldMap getMinMax sumSets')
-      solve (x:curList) (sz+1) candidates' sumSets'
-
-{-
   Note 1: but listing candidates as [50,49...],
   we managed to get one solution: [26,37,43,46,48,49,50],
   so at least we have found an upper bound of the final solution.
@@ -94,9 +33,13 @@ solve curList sz candidates sumSets
   The reason for this is that:
   we already found a close upperbound for the max number,
   and want to find the first solution, which got to be the optimum.
+
+  Note 4: `findSpecialSubsets` is originally implemented in this module
+  and then moved to Problem105, because the function in Problem105
+  is more general than the version that was here.
  -}
 
 result :: Int
 result =
   read . concatMap show . head
-  $ solve [] 0 [1..46] [IS.singleton 0]
+  $ findSpecialSubsets 7 [1..46]
--- src/ProjectEuler/Problem105.hs
+++ src/ProjectEuler/Problem105.hs
@@ -1,5 +1,6 @@
 module ProjectEuler.Problem105
   ( problem
+  , findSpecialSubsets
   ) where
 
 import Control.Monad
@@ -15,41 +16,87 @@ import ProjectEuler.GetData
 problem :: Problem
 problem = pureProblemWithData "p105_sets.txt" 105 Solved compute
 
+{-
+  Note: "findSpecialSubsets" together with auxiliary functions
+  are originally implemented in ProjectEuler.Problem103,
+  they are then moved here and backported back to Problem103
+  because this version is more general than one used in Problem103.
+ -}
+
+{-
+  like "pick", but whenever an element picked,
+  all elements before it will be dropped. This has the effect of only picking
+  elements in order.
+ -}
+{-# INLINABLE pickInOrder #-}
 pickInOrder :: [a] -> [] (a,[a])
 pickInOrder = fmap (\(x:xs) -> (x,xs)) . init . tails
 
+{-# INLINABLE isIncreasing #-}
 isIncreasing :: Ord a => [a] -> Bool
 isIncreasing xs = and $ zipWith (<) xs (tail xs)
 
-{-
-  TODO:
-  we ported from Problem102, but at some point in time we should
-  share code for this.
- -}
+{-# INLINABLE findSpecialSubsets #-}
+findSpecialSubsets :: Int -> [Int] -> [[Int]]
+findSpecialSubsets solSz initCandidates =
+    solve [] 0 initCandidates [IS.singleton 0]
+  where
+    {-
+      - candidates: list of not yet used candidates
+      - sumSets: list of sum set:
+        + sumSets !! 0 => always a singleton set {0}
+        + sumSets !! 1 => set of sum of subsets of size 1
+        + sumSets !! 2 => set of sum of subsets of size 2
+        + etc.
+     -}
+    solve :: [Int] -> Int -> [Int] -> [IS.IntSet] -> [[Int]]
+    solve curList sz candidates sumSets
+      | sz == solSz = pure (reverse curList)
+      | otherwise = do
+          (x,candidates') <- pickInOrder candidates
+          let sumSetsWithX =
+                -- since we know this map is strictly monotonic,
+                -- we can bypass checks by reconstructing altogether.
+                IS.fromDistinctAscList . fmap (+x) . IS.toAscList
+                  <$> sumSets
+              sumSets' =
+                zipWith
+                  IS.union
+                  (sumSets <> [IS.empty])
+                  (IS.empty : sumSetsWithX)
+              allSums' = foldl' IS.union IS.empty sumSets'
+              getMinMax s
+                | IS.null s = []
+                | IS.size s == 1 = [IS.findMin s]
+                | otherwise = [IS.findMin s, IS.findMax s]
 
-solve :: Int -> [Int] -> Int -> [Int] -> [IS.IntSet] -> [[Int]]
-solve solSz curList sz candidates sumSets
-  | sz == solSz = pure (reverse curList)
-  | otherwise = do
-      (x,candidates') <- pickInOrder candidates
-      let sumSetsWithX =
-            IS.fromDistinctAscList . fmap (+x) . IS.toAscList <$> sumSets
-          sumSets' = zipWith IS.union (sumSets <> [IS.empty]) (IS.empty : sumSetsWithX)
-          allSums' = foldl' IS.union IS.empty sumSets'
-          getMinMax s
-            | IS.null s = []
-            | IS.size s == 1 = [IS.findMin s]
-            | otherwise = [IS.findMin s, IS.findMax s]
-      guard $ IS.size allSums' == 1 `unsafeShiftL` (sz+1)
-      guard $ isIncreasing (foldMap getMinMax sumSets')
-      solve solSz (x:curList) (sz+1) candidates' sumSets'
+          -- a note about a simple trick:
+          -- most of the time it's faster to write down guards
+          -- separately rather than combining them with `&&`.
+          -- I'm not sure why this is the case though,
+          -- for now my theory is that `&&`  does has some extra cost
+          -- of doing pattern matching before return value to `guard`,
+          -- but I'm not convinced by my own theory and believe ghc should
+          -- take care of nested pattern matching (one in `&&` and one in `guard`).
+
+          -- to make sure that all sums are unique, we can simply verify that
+          -- the # of elements in list is expected.
+          guard $ IS.size allSums' == 1 `unsafeShiftL` (sz+1)
+          -- for the size vs sum condition to hold, we just want to know
+          -- if we were to extra min and max from each element of sumSets'
+          -- (of course for singleton sets we only need one element)
+          -- do we end up with an increasing sequence
+          -- or, in other words, this is the same as asking that
+          -- the range of each pair of neighborhood elements in allSums'
+          -- shouldn't overlap.
+          guard $ isIncreasing (foldMap getMinMax sumSets')
+          solve (x:curList) (sz+1) candidates' sumSets'
 
 getSpecialSubsetSum :: [Int] -> Sum Int
-getSpecialSubsetSum xs = case solve l [] 0 xs [IS.singleton 0] of
+getSpecialSubsetSum xs =
+  case findSpecialSubsets (length xs) xs of
     [] -> 0
     x:_ -> Sum $ sum x
-  where
-    l = length xs
 
 compute :: T.Text -> Int
 compute =
@@ -58,5 +105,5 @@ compute =
     . lines
     . T.unpack
   where
-    parseLine raw = read ("[" <> raw <> "]") :: [Int]
+    parseLine raw = read ("[" <> raw <> "]")
 
