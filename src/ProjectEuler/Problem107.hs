a6f03413d8934b20356ce066a36424c7eccb72a4
Javran Cheng <Javran.c@gmail.com>
2019-07-20 23:49:58 -0700

    p107: idea.

--- src/ProjectEuler/Problem107.hs
+++ src/ProjectEuler/Problem107.hs
@@ -2,11 +2,25 @@ module ProjectEuler.Problem107
   ( problem
   ) where
 
-import ProjectEuler.Types
+import qualified Data.Text as T
+
+import ProjectEuler.GetData
+
+{-
+  Surely Dijkstra's algorithm should do, so why don't we try something new,
+  say Prim's algorithm.
+
+  This will involve:
+  - a heap to pop up edges of minimal weights
+  - disjoint set so we can easily tell when to stop
+    (for now I'm thinking about size-based disjoint set,
+    so that we can look at any node and see if the root set has contained
+    all nodes as the condition of terminating the algorithm.)
+ -}
 
 problem :: Problem
-problem = pureProblem 107 Unsolved result
+problem = pureProblemWithData "p107_network.txt" 107 Unsolved compute
 
-result = ()
-
+compute :: T.Text -> ()
+compute _ = ()
 
