aa5f9a87c197c82bf8a95100d0b3b7fb75d8dbed
Javran Cheng <Javran.c@gmail.com>
2019-07-21 23:16:27 -0700

    p107: some initial impl

--- src/ProjectEuler/Problem107.hs
+++ src/ProjectEuler/Problem107.hs
@@ -6,11 +6,16 @@ module ProjectEuler.Problem107
   ( problem
   ) where
 
+import Data.Function
 import Data.Maybe
 import Data.List
+import Control.Monad.ST
+import Control.Monad
 
 import qualified Data.Text as T
 import qualified Data.PSQueue as PSQ
+import qualified Data.UnionFind.ST as UF
+import qualified Data.Vector.Mutable as MVec
 
 import ProjectEuler.GetData
 
@@ -31,8 +36,44 @@ problem = pureProblemWithData "p107_network.txt" 107 Unsolved compute
 
 type Coord = (Int, Int)
 
--- compute :: T.Text -> ()
-compute raw = show $ unfoldr PSQ.minView psq
+vertices :: [Int]
+vertices = [0..39]
+
+prim'sAlgorithm :: PSQ.PSQ Coord Int -> Int
+prim'sAlgorithm initPsq = runST $ do
+  -- `clsSz ! v` stores equivalence class size of a vertex
+  -- note that the data is only valid when the vertex is the representative.
+  clsSz <- MVec.replicate 40 (1 :: Int)
+  uf <- MVec.unsafeNew 40
+  -- initialize union-find-set
+  forM_ vertices $ \v -> do
+    s <- UF.fresh v
+    MVec.write uf v s
+  (fix $ \loop psq result -> do
+           sz0 <- MVec.read clsSz 0
+           if sz0 == 40
+             then pure result
+             else
+               case PSQ.minView psq of
+                 Nothing -> error "no more edge to try."
+                 Just ((x,y) PSQ.:-> w, psq') -> do
+                   px <- MVec.read uf x
+                   py <- MVec.read uf y
+                   connected <- UF.equivalent px py
+                   if connected
+                     then loop psq' result
+                     else do
+                       UF.union' px py $ \_ _ -> do
+                         szX <- MVec.read clsSz x
+                         szY <- MVec.read clsSz y
+                         MVec.write clsSz y (szX + szY)
+                         pure y
+                       loop psq' (result + w)
+
+    ) initPsq (0 :: Int)
+
+compute :: T.Text -> Int
+compute raw = prim'sAlgorithm psq
   where
     psq :: PSQ.PSQ Coord Int
     psq = PSQ.fromList $ foldMap mkBinding weights
@@ -47,7 +88,7 @@ compute raw = show $ unfoldr PSQ.minView psq
         Nothing -> pure False
     weights :: [(Coord, Int)]
     weights =
-        catMaybes $ zipWith (\c t -> (c,) <$> t) [(r,c) | r <- [0..39], c <- [0..39]] parsed
+        catMaybes $ zipWith (\c t -> (c,) <$> t) [(r,c) | r <- vertices, c <- vertices] parsed
       where
         parsed :: [Maybe Int]
         parsed = concatMap (fmap parse . T.splitOn ",") . T.lines $ raw
