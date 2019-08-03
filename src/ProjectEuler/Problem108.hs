a46130de8aa5cd9125ddd8d876264bc4d48f3fa0
Javran Cheng <Javran.c@gmail.com>
2019-08-02 23:17:01 -0700

    p108: some more observations.

--- src/ProjectEuler/Problem108.hs
+++ src/ProjectEuler/Problem108.hs
@@ -25,8 +25,32 @@ problem = pureProblem 108 Solved result
   so we have search space `n+1 <= x <= 2*n` that we can search for solutions,
   my plan is to just brute force that and see if we can find any clues on this.
 
+  Actually initial search gives exactly what we want: https://oeis.org/A018892:
 
-  Actually initial search gives exactly what we want: https://oeis.org/A018892.
+  a(n) = (tau(n^2)+1)/2
+
+  For prime numbers p1, p2, ..., pt we have:
+
+  - If n = (p1^a1)(p2^a2)...(pt^at)
+  - then a(n) = ((2*a1 + 1)(2*a2 + 1) ... (2*at + 1) + 1)/2
+
+  Note that the value of primes (p1, p2, ..., pt) doesn't actually contribute
+  to a(n), so to minimize n, we just need to take primes in ascending order.
+  (this is by no means a estabilished conclusion, just my intuition for now).
+
+  To find the minimum n, we can first "reverse engineer" from a(n),
+  find the sequence `a1,a2,...,at` that exceeds 1000, and gradually narrow
+  down the search space:
+
+  let a1=a2=...=at=1,
+  (3^t + 1) / 2 > 1000
+  > 3^t + 1 > 2000
+  > t >= 7
+
+  So product [2,3,5,7,11,13,17] = 510510 is definitely **a** solution,
+  but not the minimal one.
+
+  (TODO: how do we find the minimal one?)
 
  -}
 
