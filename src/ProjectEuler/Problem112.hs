91385bda524f05bd24d1e67b9553e617ca35ce6d
Javran Cheng <Javran.c@gmail.com>
2019-08-05 23:27:17 -0700

    p112: solved.

--- src/ProjectEuler/Problem112.hs
+++ src/ProjectEuler/Problem112.hs
@@ -1,16 +1,41 @@
+{-# LANGUAGE BangPatterns #-}
 module ProjectEuler.Problem112
   ( problem
   ) where
 
+import Data.List
+import Petbox
+
 import ProjectEuler.Types
 
 problem :: Problem
-problem = pureProblem 112 Unsolved result
+problem = pureProblem 112 Solved result
 
 {-
-  stupid idea: let's see if brute force works here..
+  turns out brute forcing this is good enough..
  -}
+isNondecreasing :: [Int] -> Bool
+isNondecreasing xs = and (zipWith (<=) xs (tail xs))
 
-result = ()
+isBouncy :: Int -> Bool
+isBouncy z = not (isNondecreasing xs) && not (isNondecreasing ys)
+  where
+    xs = intToDigits z
+    ys = intToDigitsRev z
 
+partitionCount :: (a -> Bool) -> [a] -> [(Int, Int)]
+partitionCount f =
+  scanl' (\(!tCnt,!fCnt) x -> if f x then (tCnt+1,fCnt) else (tCnt,fCnt+1)) (0,0)
+
+result :: Int
+result = u + v
+  where
+   {-
+     We want: x / x+y == 99 / 100 => x+y /= 0 and x == 99 * y.
+     so we just want to take the second value from partitionCount
+     (first one is the initial value, namely (0,0))
+    -}
+   _:(u,v):_ =
+     filter (\(x,y) -> x == 99 * y)
+     $ partitionCount isBouncy [1..]
 
