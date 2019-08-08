d0633d111f272b0c1b8562186ef884bfb480f39b
Javran Cheng <Javran.c@gmail.com>
2019-08-07 22:08:39 -0700

    p113: proof of concept.

--- src/ProjectEuler/Problem113.hs
+++ src/ProjectEuler/Problem113.hs
@@ -21,6 +21,8 @@ import ProjectEuler.Types
 
   f(l,d) = sum of f(l-1,d') where 0 <= d' <= d
 
+  TODO: this can be sped up by deriving f(l,d) from f(l-1,d) and f(l,d-1)
+
   now let g(l,d) be the number of non-increasing number of length l.
 
   g(1,0) = 0
@@ -41,6 +43,30 @@ import ProjectEuler.Types
 problem :: Problem
 problem = pureProblem 113 Unsolved result
 
-result = ()
+f :: Int -> Int -> Int
+f 1 0 = 0
+f 1 _ = 1
+f l d = sum [f (l-1) d' | d' <- [0..d]]
 
+g :: Int -> Int -> Int
+g 1 0 = 0
+g 1 _ = 1
+g l d = sum [g (l-1) d' | d' <- [d..9]]
+
+h :: Int -> Int
+h l = sum [f l d + g l d | d <- [0..9]] - 9
+
+{-
+
+  Update: the proof of concept works for 6 digits and 10 digits:
+
+  > sum [h l | l <- [1..6]]
+  12951
+  > sum [h l | l <- [1..10]]
+  277032
+
+ -}
+
+result :: Int
+result = sum [h l | l <- [1..10]]
 
