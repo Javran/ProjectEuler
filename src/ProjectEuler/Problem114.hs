7733481a0eb461b528b6222c57f1ab945e8604a5
Javran Cheng <Javran.c@gmail.com>
2019-08-13 21:39:41 -0700

    p114: some cleanup & speedup.

--- src/ProjectEuler/Problem114.hs
+++ src/ProjectEuler/Problem114.hs
@@ -5,7 +5,7 @@ module ProjectEuler.Problem114
   ( problem
   ) where
 
-import Control.Monad.State
+import Control.Monad.State.Strict
 import qualified Data.IntMap as IM
 
 import ProjectEuler.Types
@@ -14,7 +14,7 @@ problem :: Problem
 problem = pureProblem 114 Solved result
 
 {-
-  Idea: perhaps dynamic programming is possible on this one:
+  Idea: Let's do dynamic programming on this one:
 
   Let f(i) be the ways to occupy from 1 to i (inclusive),
   and all these ways has to:
@@ -25,7 +25,8 @@ problem = pureProblem 114 Solved result
   therefore:
 
   f(i) = 0 (i < 3)
-  f(3) = 1 [XXX]
+  f(3) = 1
+       [XXX]
   f(4) = 2
        [XXXX]
     or [_XXX]
@@ -47,50 +48,42 @@ problem = pureProblem 114 Solved result
     or [XXX_XXX]
 
   then the solution should be 1 + sum of f(i) for all i,
-  and the missing case corresponds to "no block at all".
+  and the missing case (the "1 +" bit) corresponds to "no block at all".
 
   To avoid counting duplicated cases, let's consider
   the final block being of length l:
   (from problem description we know l >= 3)
 
-  - note that for l = 3 to l = i, we can do a "nothing but last block" case,
+  - note that for all l = 3 to i, we can do a "nothing but last block" case,
     which accounts for i-3+1 = i-2 cases.
-  - besides the case above, for 3 <= j <= i-l-1, we can sum up f(j) for  3 <= l <= i
+  - besides the case above, for 3 <= j <= i-l-1, we can sum up f(j) for 3 <= l <= i.
 
- -}
-
-{-
+original implementation for this is:
 
 f i
   | i < 3 = 0
   | i == 3 = 1
   | otherwise =
       let g :: Int -> Int
-          g l = sum (f <$> [3,4..i-l-1])
-      in (i-2) + sum (g <$> [3,4..i])
+          g l = sum (f <$> [3..i-l-1])
+      in (i-2) + sum (g <$> [3..i])
  -}
 
 {-
-  TODO: see if we can somehow simplify f
-    so we don't need to deal with "nothing at all" case specially.
   TODO: abstract out memoization logic?
  -}
 f :: Int -> State (IM.IntMap Integer) Integer
 f i
   | i < 3 = pure 0
-  | i == 3 = pure 1
   | otherwise =
       gets (IM.lookup i) >>= \case
         Nothing -> do
           let g :: Int -> State (IM.IntMap Integer) Integer
-              g l = do
-                xs <- mapM f [3,4..i-l-1]
-                pure (sum xs)
-          ys <- mapM g [3,4..i]
-          let r = fromIntegral (i-2) + sum ys
-          modify (IM.insert i r)
-          pure r
-        Just v -> pure (v :: Integer)
+              g l = sum <$> mapM f [3..i-l-1]
+          cnt <- sum <$> mapM g [3..i]
+          let r = fromIntegral (i-2) + cnt
+          r <$ modify (IM.insert i r)
+        Just v -> pure v
 
 result :: Integer
 result = 1 + sum (evalState (mapM f [1..50]) IM.empty)
