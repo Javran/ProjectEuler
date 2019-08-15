11d237cf4896236e5db20e5833bc177e53a8fdbc
Javran Cheng <Javran.c@gmail.com>
2019-08-14 23:43:27 -0700

    p116: some symbol pushing stuff.
    I feel we are fairly close to the solution already.

--- src/ProjectEuler/Problem116.hs
+++ src/ProjectEuler/Problem116.hs
@@ -15,6 +15,28 @@ problem = pureProblem 116 Unsolved result
   there are n+1 gaps where we can insert empty cells into them
   to reach the desired row length. I suspect Stirling numbers will
   somehow be involved.
+
+  Let f(m,n) be the number of ways to fill a row measuring n,
+  with tiles each measuring m.
+
+
+  Let g(m,n,k) be the number of ways to fill a row measuring n,
+  with k tiles each measuring m.
+
+  Therefore:
+
+  f(m,n) = sum of g(m,n,k) for k from 1 to floor(n/m)
+
+  For g(m,n,k), there are k+1 gaps that we can fill empty cells
+  to construct a row measuring n, and the number of empty cells
+  needed is n - k*m.
+
+  In other words, g(m,n,k) is also the way to put n-k*m empty cells
+  into k+1 gaps while some gaps are allowed to be empty.
+  (also all empty cells are identical, so permutation doesn't count)
+
+  While Stirling numbers describes the way to put x elements into y non-empty subsets,
+  we can workaround this restriction by fixing y from 1 to k+1 and sum the result up.
  -}
 
 result = ()
