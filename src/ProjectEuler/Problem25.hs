2beb5853d72c8c67274c9fc03bb32d07e50f3fd5
Javran Cheng <Javran.c@gmail.com>
2019-08-18 22:59:16 -0700

    p25: digitLen needs some fixing.

--- src/ProjectEuler/Problem25.hs
+++ src/ProjectEuler/Problem25.hs
@@ -2,6 +2,8 @@ module ProjectEuler.Problem25
   ( problem
   ) where
 
+-- import Petbox
+
 import ProjectEuler.Types
 
 problem :: Problem
@@ -15,5 +17,6 @@ fib x = nfibs 2 !! x
 
 result :: Int
 -- TODO: magic number?
-result = head [x | x <-[4751..], (length . show . fib) x >=1000 ]
+-- TODO: digitLen not working? (perhaps the number is too large)
+result = head [x | x <-[4751..], (length . show . fib) x >= 1000 ]
 
