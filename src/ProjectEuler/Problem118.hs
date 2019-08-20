6ecb60f69044dce3f8b8c129742cc69aea6c5a57
Javran Cheng <Javran.c@gmail.com>
2019-08-19 22:54:23 -0700

    p118: promising so far.

--- src/ProjectEuler/Problem118.hs
+++ src/ProjectEuler/Problem118.hs
@@ -2,8 +2,11 @@ module ProjectEuler.Problem118
   ( problem
   ) where
 
-import Petbox (pick)
+import Petbox (pick, intToDigits)
 import Math.NumberTheory.Primes.Testing (isPrime)
+import qualified Data.Map.Strict as M
+import qualified Data.IntSet as IS
+import Data.Monoid
 import ProjectEuler.Types
 
 problem :: Problem
@@ -14,12 +17,23 @@ problem = pureProblem 118 Unsolved result
   if we can list all primes whose every digit is unique,
   then we basically need to compute a set that has all digits
   being covered exactly once.
+
+  one idea worth noting: if number a,b,c,... all has the same set of digits,
+  we don't actually need to count them one-by-one.
+  so if we organize these primes by the set of digits they have,
+  we can save quite a bit of computation.
+
  -}
 
+-- 43089 primes that we need to consider.
 genPrimes :: Int -> [Int] -> [Int]
 genPrimes acc candidates = do
   (d, candidates') <- pick candidates
   let acc' = acc*10 + d
   [acc' | isPrime (fromIntegral acc')] <> genPrimes acc' candidates'
 
-result = length (genPrimes 0 [1..9]) -- 43089 primes that we need to consider.
+result =
+  -- now we only have 308 elements to do set-cover.
+  M.size
+  . M.fromListWith (<>)
+  $ (\n -> (IS.fromList $ intToDigits n, 1 :: Sum Int)) <$> genPrimes 0 [1..9]
