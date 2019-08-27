3acea10561023acba44729e1b8b5c60e09befe6d
Javran Cheng <Javran.c@gmail.com>
2019-08-26 22:37:56 -0700

    p120: a bit more analysis on this one.

--- src/ProjectEuler/Problem120.hs
+++ src/ProjectEuler/Problem120.hs
@@ -33,6 +33,40 @@ problem = pureProblem 120 Solved result
 
   For now it looks like for any n, the max r can be found by trying
   just n <- [0..a*2-1].
+
+
+  Some more insights on this one:
+
+  let B(n,k) denote "n choose k" (a.k.a binomial coefficient)
+
+  (a + 1)^n = sum{ B(n,i)*a^(n-i)*1^i, i = 0 to n }
+  (a - 1)^n = sum{ B(n,i)*a^(n-i)*(-1)^i, i = 0 to n }
+
+  notice that these two expansion has same number of terms, and when i is odd,
+  some terms will cancel each other, leaving:
+
+  (a + 1)^n + (a - 1)^n = 2 * sum{ B(n,i)*a^(n-i), i = 0 to n && i is even }
+
+  note that a^k mod a^2 = 0 for all k >= 2, therefore:
+
+  [(a + 1)^n + (a - 1)^n] (mod a^2)
+  = 2 * sum{ B(n,i)*a^(n-i), i = 0 to n && i is even } (mod a^2)
+  = 2 * sum{ B(n,i)*a^(n-i), i = 0 to n && i is even && n - i < 2} (mod a^2)
+  (n - i < 2 restricts i to only 2 possible values: i = n or i = n - 1.)
+  = 2 * sum{ B(n,i)*a^(n-i), i = n-1 or n && i is even } (mod a^2)
+
+  when n is even:
+  [(a + 1)^n + (a - 1)^n] (mod a^2)
+  = 2 * sum{ B(n,i)*a^(n-i), i = n } (mod a^2)
+  = 2 * B(n,n) (mod a^2)
+  = 2 (mod a^2)
+
+  when n is odd:
+  [(a + 1)^n + (a - 1)^n] (mod a^2)
+  = 2 * sum{ B(n,i)*a^(n-i), i = n-1 } (mod a^2)
+  = 2 * B(n,n-1) * a (mod a^2)
+  = 2 * n * a (mod a^2)
+
  -}
 
 f :: Int -> Int -> Int
