2571fa3bdfa268711d41e93354d5cd558627713c
Javran Cheng <Javran.c@gmail.com>
2019-08-27 23:27:16 -0700

    p124: solved.

--- src/ProjectEuler/Problem124.hs
+++ src/ProjectEuler/Problem124.hs
@@ -2,10 +2,16 @@ module ProjectEuler.Problem124
   ( problem
   ) where
 
+import Math.NumberTheory.Primes.Factorisation
+import Control.Monad
+
+import qualified Data.List.Ordered as LOrdered
+import qualified Petbox
+import qualified Data.IntMap.Strict as IM
 import ProjectEuler.Types
 
 problem :: Problem
-problem = pureProblem 124 Unsolved result
+problem = pureProblem 124 Solved result
 
 {-
   Idea: instead of enumerating all numbers in range
@@ -17,8 +23,35 @@ problem = pureProblem 124 Unsolved result
   - note that in the sorted results, numbers from same radical
     is always groupped together, we can take advantage of this property
     to skip through elements.
+
+  Update: generating them take more time, experimenting factorization on all of them.
+
  -}
 
-result = ()
+maxN :: Int
+maxN = 100000
 
+-- for this particular problem, these are all primes that we need to consider.
+primes :: [Int]
+primes = takeWhile (< maxN) Petbox.primes
 
+-- NOTE: this might not be as practical as I thought - generating them takes more time.
+_genRadicals curProduct curList candidates = do
+  (c, candidates') <- Petbox.pickInOrder candidates
+  let curProduct' = c * curProduct
+  guard $ curProduct' < maxN
+  [(curProduct', c:curList)] <> _genRadicals curProduct' (c:curList) candidates'
+
+calcRadical :: Int -> Int
+calcRadical x = product (fromIntegral . fst <$> factorise (fromIntegral x))
+
+{- TODO: cleanup -}
+
+result =
+    (!! 9999)
+    . foldMap snd
+    . IM.toAscList
+    $ IM.fromListWith LOrdered.union (toPair <$> [1..maxN])
+  where
+    toPair :: Int -> (Int, [Int])
+    toPair x = (calcRadical x, [x])
