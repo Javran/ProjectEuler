5eae0946fb80bb16ed92546b0929049494ff860f
Javran Cheng <Javran.c@gmail.com>
2019-08-29 23:16:25 -0700

    p23: impl factorise :: Int -> Int
    see if specialized ones are more efficient.

--- src/ProjectEuler/Problem23.hs
+++ src/ProjectEuler/Problem23.hs
@@ -3,13 +3,32 @@ module ProjectEuler.Problem23
   ) where
 
 import Math.NumberTheory.Primes.Factorisation (factorise)
+import qualified Math.NumberTheory.Primes as Primes
 import Data.Monoid
+import Control.Arrow
 import qualified Data.IntSet as IS
-
 import ProjectEuler.Types
 
 problem :: Problem
 problem = pureProblem 23 Solved result
+
+maxAbun :: Int
+maxAbun = 28123
+
+primes' :: [Int]
+primes' = takeWhile (<= maxAbun) $ Primes.unPrime <$> Primes.primes
+
+factorise1 :: Int -> [(Int, Int)]
+factorise1 = auxFactor primes'
+  where
+    auxFactor [] 1 = []
+    auxFactor [] _ = error "unreachable"
+    auxFactor (p:ps) n
+      | n `rem` p == 0 =
+          let pPows = takeWhile ((<=n) . snd) $ iterate (succ *** (*p)) (1,p)
+              (count, pz) = last $ filter (\(_,p') -> n `rem` p' == 0) pPows
+          in (p,count) : auxFactor ps (n `quot` pz)
+      | otherwise = auxFactor ps n
 
 -- see: http://mathschallenge.net/library/number/sum_of_divisors
 divisorSum :: Int -> Int
@@ -28,7 +47,7 @@ result =
     getSum $ foldMap (\x -> if IS.member x reachables then 0 else Sum x) [1..maxAbun]
   where
     possibleAbuns = filter isAbundant [1..maxAbun]
-    maxAbun = 28123
+
     reachables =
       IS.fromList $ do
         x <- possibleAbuns
