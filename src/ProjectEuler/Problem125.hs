c8c9623baef0ed701e5b170da754ae420458d7b1
Javran Cheng <Javran.c@gmail.com>
2019-10-31 23:56:45 -0700

    p125: change of plan.
    original one doesn't quite work as we've estimated it wrong.
    however, this gives us some good intuition to work with,
    an improved one on its way.

--- src/ProjectEuler/Problem125.hs
+++ src/ProjectEuler/Problem125.hs
@@ -2,6 +2,8 @@ module ProjectEuler.Problem125
   ( problem
   ) where
 
+import Petbox
+
 import ProjectEuler.Types
 
 problem :: Problem
@@ -17,7 +19,7 @@ problem = pureProblem 125 Unsolved result
 
   To find the upperbound:
 
-  > let f n = (n * (n+1) * (2*n+1)) `div` 6
+  > let f n = (n * (n+1) * (2*n+1)) `quot` 6
   > f 1
   1
   > f 2
@@ -29,8 +31,35 @@ problem = pureProblem 125 Unsolved result
 
   Doesn't sound like a lot to just brute force.
 
+  Update: not so fast though. The upperbound does not work this way:
+  we want to actually upperbound on the difference, meaning
+  that the actual sum might be way exceeding 10^8,
+  perhaps we can try fixing the initial number to i,
+  try i^2 + (i+1)^2 ... until it exceeds the maximum,
+  then try (i+1)^2 as the initial one.
+
  -}
 
-result = ()
+-- ported from p55
+isPalindrome :: Int -> Bool
+isPalindrome x = x == numReverse x
 
+numReverse :: Int -> Int
+numReverse = numReverseInBase 10
 
+maxV :: Int
+maxV = 100000000
+
+partialAns :: Int -> [Int]
+partialAns n = filter isPalindrome sqSum
+  where
+    sqSum =
+      takeWhile (< maxV)
+      . drop 1 -- dropping the first one to limit it to more than one number
+      . scanl1 (+)
+      $ (\i -> i*i) <$> [n..]
+
+result :: Int -- TODO: could it be possible that the generated list is containing non-uniques?
+result = sum $ do
+  i <- [1 .. 10000]
+  partialAns i
