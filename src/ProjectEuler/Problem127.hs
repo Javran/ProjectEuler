2e0e141750834d89366f7b621e364ddc9506b7c5
Javran Cheng <Javran.c@gmail.com>
2019-11-07 21:19:33 -0800

    p127: listing some potential improvements.

--- src/ProjectEuler/Problem127.hs
+++ src/ProjectEuler/Problem127.hs
@@ -26,7 +26,25 @@ problem = pureProblem 127 Unsolved result
 
   - c = a + b, if c < 1000, we know a + b < 1000
 
-  - c = a + b and a < b, therefore c < 2b
+  - c = a + b and a < b, therefore c < 2*b
+
+  - b > 2, because:
+
+    + when b = 2, (a,b,c) = (1,2,3), in which rad(a*b*c) < c does not hold.
+    + b > 1, otherwise there is no value for a to take.
+
+  - rad(c) >= 2, this is simply because c > b > 2.
+
+  - rad(c) >= 2 && c < 2*b && rad(a)*rad(b)*rad(c) < c therefore:
+
+    rad(a)*rad(b)*rad(c) < c < 2 * b <= rad(c) * b
+    => rad(a) * rad(b) < b
+
+    TODO: there are some rooms of improvement from this fact:
+    - precompute  rad(_), create a mapping from rad(x) to list of x.
+    - if we start with searching b, we can constrain a to a very limited search space.
+    - note that gcd(rad(a), rad(b)) = 1 iff. gcd(a,b) = 1, therefore
+      we gain more speed by this groupping on rad(_), as there are less coprime tests to do.
 
   Update: now the example (c < 1000) given by the problem is working,
   but it is too slow to simply plugging in 120000, we need to do something else.
@@ -56,10 +74,11 @@ rad = (radVec V.!)
 
 searchAbcHits :: [(Int, Int, Int)]
 searchAbcHits = do
-  b <- [2..maxN]
+  b <- [3..maxN]
+  let rb = rad b
   -- c < maxN => a + b < maxN => a <= maxN - b - 1
-  a <- filter (coprime b) [1..min (b-1) (maxN-b-1)]
-  let rab = rad a * rad b
+  a <- filter (\a -> coprime a b && rad a * rb < b) [1..min (b-1) (maxN-b-1)]
+  let rab = rad a * rb
       c = a + b
   {-
     At this point we know a and b does not share any factor,
