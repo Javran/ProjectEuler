db700d493e5846074b764f8d38b854085d3dd269
Javran Cheng <Javran.C@gmail.com>
2019-11-21 23:17:27 -0800

    p128: done cleaup & optimize.

--- src/ProjectEuler/Problem128.hs
+++ src/ProjectEuler/Problem128.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, OverloadedStrings #-}
+{-# LANGUAGE
+    DeriveFunctor
+  , DeriveFoldable
+  , OverloadedStrings
+  #-}
 module ProjectEuler.Problem128
   ( problem
   ) where
 
-import Data.Foldable
-import Data.Functor
+import Data.List
 import Data.Maybe
 import Math.NumberTheory.Primes
 
@@ -79,9 +82,12 @@ problem = pureProblem 128 Solved result
 
   - With those discoveries above, we managed to get the correct answer - more optimization could follow.
 
+  - Turns out it is only possible to obtain PD(_) = 3 for the first and last cell of a ring,
+    so I optimized my code to only check for that.
+
  -}
 
-type AxialCoord = (Int, Int) -- coordinate
+type AxialCoord = (Int, Int)
 type AxialDir = (Int, Int)
 
 -- unit directions following tiles' growing direction.
@@ -99,23 +105,7 @@ plus :: AxialDir -> AxialDir -> AxialDir
 plus (x,y) (a,b) = (x+a, y+b)
 
 dist :: AxialCoord -> AxialCoord -> Int
-dist (ax, az) (bx, bz) =
-    abs (ax - bx) `max` abs (ay - by) `max` abs (az - bz)
-  where
-    ay = -ax - az
-    by = -bx - bz
-
-{-
-  If we number the center cell / ring as "ring #0",
-  and walk our way outwards around,
-  "genCoords n" generates coordinates for "ring #n" for us.
- -}
-genCoords :: Int -> [AxialCoord]
-genCoords 0 = [(0,0)]
-genCoords n = take (6*n) coords
-  where
-    coords = scanl plus (0,-n) dirs
-    dirs = concatMap (replicate n) unitDirs
+dist (ax, az) (bx, bz) = abs (ax - bx) `max` abs (-ax -az + bx + bz) `max` abs (az - bz)
 
 data HC a = HC !a !a !a !a !a !a deriving (Functor, Foldable)
 
@@ -129,44 +119,44 @@ hcInd (HC v0 v1 v2 v3 v4 v5) i = case i of
   5 -> v5
   _ -> error "out of bound"
 
-mkHexCorners :: Int -> HC (AxialCoord, Int)
-mkHexCorners 0 = let z = ((0,0), 1) in HC z z z z z z
-mkHexCorners n =
-  let vInit = 3*n*n - 3*n + 2
-  in HC ((0, -n), vInit)
-        ((-n,0), vInit+n)
-        ((-n,n), vInit+n*2)
-        ((0,n), vInit+n*3)
-        ((n,0), vInit+n*4)
-        ((n,-n), vInit+n*5)
+{- Compute coords of corner cells of a ring. -}
+mkHexCorners :: Int -> HC AxialCoord
+mkHexCorners 0 = let z = (0,0) in HC z z z z z z
+mkHexCorners n = HC (0,-n) (-n,0) (-n,n) (0,n) (n,0) (n,-n)
 
+{-
+  After we got the correct answer, I noticed that it seems true that
+  only first and last cell of a ring (first cell is also the top corner of that ring)
+  can have PD(_) = 3, perhaps there are proofs but I'll just assume that is true
+  and optimize things below.
+ -}
 checkAroundHexCorners :: Int -> [Int]
+checkAroundHexCorners 0 = [1]
 checkAroundHexCorners n =
-    maybeToList (computePdGreaterEqual3 tcPrevCoord)
-    <> mapMaybe (computePdGreaterEqual3 . fst) [tc,c1,c2,c3,c4,c5]
-  where
-    tcPrevCoord = fst tc `plus` (0,1) `plus` (1,0)
-    HC tc c1 c2 c3 c4 c5 = mkHexCorners n
+  mapMaybe
+    computePdGreaterEqual3
+    [ (0,-n) -- first coord of the ring.
+    , (1,-n) -- last coord of the ring.
+    ]
 
-mkPairs :: (t -> t -> a) -> HC t -> HC a
+mkPairs :: (t -> t -> a) -> HC t -> [a]
 mkPairs op (HC v0 v1 v2 v3 v4 v5) =
-  HC (op v0 v1) (op v1 v2) (op v2 v3) (op v3 v4) (op v4 v5) (op v5 v0)
+  [op v0 v1, op v1 v2, op v2 v3, op v3 v4, op v4 v5, op v5 v0]
 
 coordToTileNum :: AxialCoord -> Int
-coordToTileNum pt = case lookup pt (toList hcs) of
-    Just v -> v
-    _ ->
-      let dists = fmap (dist pt . fst) hcs
-          (cornerInd,_):_ =
-            filter ((== n) . snd) $ zip [0..] $ toList $ mkPairs (+) dists
-          ((_, tileNum0), offset) = (hcInd hcs cornerInd, hcInd dists cornerInd)
-      in tileNum0 + offset
+coordToTileNum (0,0) = 1
+coordToTileNum pt = tileNumBase + offset
   where
     n = dist pt (0,0)
     hcs = mkHexCorners n
+    dists = fmap (dist pt) hcs
+    Just cornerInd = elemIndex n $ mkPairs (+) dists
+    tileNumBase = 3*n*n + (cornerInd - 3)*n + 2
+    offset = hcInd dists cornerInd
 
+{- returns tile number n when PD(n) = 3 -}
 computePdGreaterEqual3 :: AxialCoord -> Maybe Int
-computePdGreaterEqual3 c = case mapMaybe (void . isPrime) diffs of
+computePdGreaterEqual3 c = case mapMaybe isPrime diffs of
     (_:_:_:_) ->
       {-
         Since we know that PD(_) <= 3, we can stop at 3 and claim PD(c) = 3,
@@ -177,15 +167,7 @@ computePdGreaterEqual3 c = case mapMaybe (void . isPrime) diffs of
     _ -> Nothing
   where
     x = coordToTileNum c
-    ys = fmap (coordToTileNum . plus c) unitDirs
-    diffs = (\y -> abs (y - x)) <$> ys
-
-fastCompute :: [Int]
-fastCompute =
-  -- here "init" is used to exclude last generated coordinate because
-  -- it will be handled by ring #4's top corner.
-  mapMaybe computePdGreaterEqual3 (init $ concatMap genCoords [0..3])
-  <> concatMap checkAroundHexCorners [4..]
+    diffs = fmap ((\y -> abs (y - x)) . coordToTileNum . plus c) unitDirs
 
 result :: Int
-result = fastCompute !! (2000 - 1)
+result = concatMap checkAroundHexCorners [0..] !! (2000 - 1)
