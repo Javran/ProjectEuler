cd499f674cb62bd4129308669e2423b298cc2943
Javran Cheng <Javran.C@gmail.com>
2019-11-22 22:58:17 -0800

    p129: creation.

--- src/ProjectEuler/AllProblems.hs
+++ src/ProjectEuler/AllProblems.hs
@@ -137,6 +137,7 @@ import ProjectEuler.Problem125
 import ProjectEuler.Problem126
 import ProjectEuler.Problem127
 import ProjectEuler.Problem128
+import ProjectEuler.Problem129
 import ProjectEuler.Problem206
 
 allProblems :: IM.IntMap Problem
@@ -270,5 +271,6 @@ allProblems =
     , (126, ProjectEuler.Problem126.problem)
     , (127, ProjectEuler.Problem127.problem)
     , (128, ProjectEuler.Problem128.problem)
+    , (129, ProjectEuler.Problem129.problem)
     , (206, ProjectEuler.Problem206.problem)
     ]
---
+++ src/ProjectEuler/Problem129.hs
@@ -0,0 +1,12 @@
+module ProjectEuler.Problem129
+  ( problem
+  ) where
+
+import ProjectEuler.Types
+
+problem :: Problem
+problem = pureProblem 129 Unsolved result
+
+result = ()
+
+
