f61ca12a5b97cd76d70c9cd5eeb641fdf53c7ebc
Javran Cheng <Javran.C@gmail.com>
2019-11-23 23:03:17 -0800

    p131: solved.

--- src/ProjectEuler/Problem131.hs
+++ src/ProjectEuler/Problem131.hs
@@ -2,13 +2,15 @@ module ProjectEuler.Problem131
   ( problem
   ) where
 
+import Data.Monoid
+import Data.Maybe
 import Control.Monad
 import Math.NumberTheory.Primes
 
 import ProjectEuler.Types
 
 problem :: Problem
-problem = pureProblem 131 Unsolved result
+problem = pureProblem 131 Solved result
 
 {-
   No idea what I'm dealing with here, might begin with finding some
@@ -16,12 +18,24 @@ problem = pureProblem 131 Unsolved result
  -}
 
 -- This initial search points me to: https://oeis.org/A002407
-result = reverse $ do
-  m <- [10000 :: Int ,9999 ..1]
+_doSearch :: [Int]
+_doSearch = reverse $ do
+  m <- [10000, 9999 .. 1]
   let mCubed = m * m * m
-  n <- [m-1,m-2..1]
+  n <- [m-1, m-2 .. 1]
   let nSq = n * n
       (q, r) = (mCubed - n * nSq) `quotRem` nSq
   guard $ r == 0
   Just _ <- pure $ isPrime q
   pure q
+
+result :: Int
+result =
+    getSum
+    . foldMap (const 1)
+    . takeWhile (< 1000000)
+    . filter (isJust . isPrime)
+    $ zipWith (-) (tail cubes) cubes
+  where
+    cubes :: [Int]
+    cubes = (\x -> x ^ (3 :: Int)) <$> [1..]
