a6ed6fd75e03dc5954e2787cc762b6effb107c98
Javran Cheng <Javran.C@gmail.com>
2019-11-26 22:19:31 -0800

    p134: a bit speed up & fix a subtle bug.

--- src/ProjectEuler/Problem134.hs
+++ src/ProjectEuler/Problem134.hs
@@ -3,22 +3,35 @@ module ProjectEuler.Problem134
   ) where
 
 import Petbox
+import Data.Word
 
 import ProjectEuler.Types
 
 problem :: Problem
 problem = pureProblem 134 Unsolved result
 
-primePairs :: [(Integer, Integer)]
-primePairs = zip ps (tail ps)
-  where
-    ps = takeWhile (<= 1000000) $ dropWhile (< 5) primes
+{-
+  Note: don't do dropWhile + takeWhile first before making the pair,
+  instead, generate the pair and then dropWhile + takeWhile on the resulting value.
 
-findConn :: Integer -> Integer -> Integer
+  This is because otherwise while we want constraints on p1 s.t. <= 1000000,
+  p2 will also unnecessarily constrainted by this,
+  resulting in the last pair (999983,1000003) being missing.
+ -}
+primePairs :: [(Word64, Word64)]
+primePairs =
+    takeWhile ((<= 1000000) . fst)
+    . dropWhile ((< 5) . fst)
+    $ zip primes (tail primes)
+
+{-
+  This version is too slow.
+ -}
+findConn :: Word64 -> Word64 -> Word64
 findConn p1 p2 = go (p2 * floor (fromIntegral (p1+mask) / fromIntegral p2 :: Double))
   where
     l = digitLen p1
-    mask :: Integer
+    mask :: Word64
     mask = 10 ^! l
     go cur =
       if cur `rem` mask == p1
@@ -28,5 +41,5 @@ findConn p1 p2 = go (p2 * floor (fromIntegral (p1+mask) / fromIntegral p2 :: Dou
 {-
   Let's try the obvious and see how it goes.
  -}
-
+result :: Word64
 result = sum $ uncurry findConn <$> primePairs
