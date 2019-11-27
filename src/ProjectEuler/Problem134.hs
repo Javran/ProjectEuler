70beb84fa05f2a5936202ed7b7a5e29e9f50dce7
Javran Cheng <Javran.C@gmail.com>
2019-11-26 21:29:33 -0800

    p134: the obvious way - too slow.

--- src/ProjectEuler/Problem134.hs
+++ src/ProjectEuler/Problem134.hs
@@ -2,11 +2,31 @@ module ProjectEuler.Problem134
   ( problem
   ) where
 
+import Petbox
+
 import ProjectEuler.Types
 
 problem :: Problem
 problem = pureProblem 134 Unsolved result
 
-result = ()
+primePairs :: [(Integer, Integer)]
+primePairs = zip ps (tail ps)
+  where
+    ps = takeWhile (<= 1000000) $ dropWhile (< 5) primes
 
+findConn :: Integer -> Integer -> Integer
+findConn p1 p2 = go (p2 * floor (fromIntegral (p1+mask) / fromIntegral p2 :: Double))
+  where
+    l = digitLen p1
+    mask :: Integer
+    mask = 10 ^! l
+    go cur =
+      if cur `rem` mask == p1
+        then cur
+        else go (cur + p2)
 
+{-
+  Let's try the obvious and see how it goes.
+ -}
+
+result = sum $ uncurry findConn <$> primePairs
