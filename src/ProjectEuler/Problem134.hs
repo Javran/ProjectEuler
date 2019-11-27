ebf35fc7e4e6c5ea3c4128b8015c65297addf19f
Javran Cheng <Javran.C@gmail.com>
2019-11-26 23:09:44 -0800

    p134: solved.

--- src/ProjectEuler/Problem134.hs
+++ src/ProjectEuler/Problem134.hs
@@ -2,13 +2,36 @@ module ProjectEuler.Problem134
   ( problem
   ) where
 
+import Math.NumberTheory.Moduli.Chinese
 import Petbox
-import Data.Word
 
 import ProjectEuler.Types
 
 problem :: Problem
-problem = pureProblem 134 Unsolved result
+problem = pureProblem 134 Solved result
+
+{-
+  The obvious way: enumerate p2 * k (k = 1, 2, ..) until we get
+  a value ending with p1. This approach is too slow to be qualified
+  as a "under one minute" solution. We must do something better.
+
+  Let's do this properly this time:
+
+  let l be the number of digits p1 has in decimal without any leading zeros
+  (i.e. l = digitLen p1), now we want to find:
+
+  p1 + 10^l * m == p2 * n == N
+
+  Suggesting that we could try Chinese remainder:
+
+  we want to find N such that:
+
+  N `mod` 10^l == p1
+  N `mod` p2 == 0
+
+  Indeed this gets us to the solution in no time.
+
+ -}
 
 {-
   Note: don't do dropWhile + takeWhile first before making the pair,
@@ -18,28 +41,17 @@ problem = pureProblem 134 Unsolved result
   p2 will also unnecessarily constrainted by this,
   resulting in the last pair (999983,1000003) being missing.
  -}
-primePairs :: [(Word64, Word64)]
+primePairs :: [(Int, Int)]
 primePairs =
     takeWhile ((<= 1000000) . fst)
     . dropWhile ((< 5) . fst)
     $ zip primes (tail primes)
 
-{-
-  This version is too slow.
- -}
-findConn :: Word64 -> Word64 -> Word64
-findConn p1 p2 = go (p2 * floor (fromIntegral (p1+mask) / fromIntegral p2 :: Double))
+findConn :: Int -> Int -> Int
+findConn p1 p2 = prod
   where
     l = digitLen p1
-    mask :: Word64
-    mask = 10 ^! l
-    go cur =
-      if cur `rem` mask == p1
-        then cur
-        else go (cur + p2)
+    Just prod = chineseCoprime (p1, 10 ^! l) (0, p2)
 
-{-
-  Let's try the obvious and see how it goes.
- -}
-result :: Word64
+result :: Int
 result = sum $ uncurry findConn <$> primePairs
