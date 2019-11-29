2765622b808ce57a93fc708e56059d2e09db6983
Javran Cheng <Javran.C@gmail.com>
2019-11-28 21:38:57 -0800

    p136: marking as solved for now.

--- src/ProjectEuler/Problem136.hs
+++ src/ProjectEuler/Problem136.hs
@@ -10,7 +10,16 @@ import ProjectEuler.Types
 import ProjectEuler.Problem135 (countSameDiffs)
 
 problem :: Problem
-problem = pureProblem 136 Unsolved result
+problem = pureProblem 136 Solved result
+
+{-
+  Using same method as in Problem135 works.
+
+  TODO: I believe there should be a better way:
+  instead of counting, we can stop trying on a specific n
+  when it has more than 1 solution.
+
+ -}
 
 result :: Int
 result =
