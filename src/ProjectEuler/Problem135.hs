ad2b0681ebc59fe2a1dc6e03e61ae7498ee4c861
Javran Cheng <Javran.C@gmail.com>
2019-11-28 21:27:40 -0800

    p135: cleanup.

--- src/ProjectEuler/Problem135.hs
+++ src/ProjectEuler/Problem135.hs
@@ -2,15 +2,11 @@ module ProjectEuler.Problem135
   ( problem
   ) where
 
-import Data.Monoid
 import Control.Monad
-import Math.NumberTheory.Powers.Squares
-import Math.NumberTheory.ArithmeticFunctions
-import Data.Word
 import Control.Monad.ST
+import Data.Monoid
+import Data.Word
 
-import qualified Data.List.Match
-import qualified Data.IntSet as IS
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Unboxed.Mutable as VUM
 
@@ -52,41 +48,27 @@ problem = pureProblem 135 Solved result
 
  -}
 
-findSameDiffs :: Int -> [] (Int, Int)
-findSameDiffs n = do
-  let lo = integerSquareRoot' (n `quot` 3)
-      (_, ds) = IS.split lo $ divisorsSmall n
-  {-
-    m > sqrt(n / 3) >= floor( sqrt(n / 3) ),
-    therefore we can safely start with lo+1.
-   -}
-  m <- IS.toList ds
-  let numer = n + m * m
-      denom = 4 * m
-  (d, 0) <- pure $ numer `quotRem` denom
-  pure (m, d)
-
 countSameDiffs :: Int -> VU.Vector Word16
 countSameDiffs maxN = runST $ do
   vec <- VUM.replicate (maxN+1) (0 :: Word16)
-  forM_ [2 .. maxN] $ \m ->
-    forM_ (takeWhile (<= maxN) [m, m+m..]) $ \n -> do
+  forM_ [2 .. maxN] $ \m -> do
+    {-
+      Since demanding that d < m is the same as demanding n < 3 * m^2,
+      we might as well do this in the first place and don't bother checking
+      d < m.
+     -}
+    let maxN' = min maxN (3 * m * m-1)
+    forM_ [m, m+m .. maxN'] $ \n -> do
       let numer = n + m * m
           denom = 4 * m
-      case numer `quotRem` denom of
-        (d, 0) | d < m -> VUM.modify vec succ n
-        _ -> pure ()
+      when (numer `rem` denom == 0) $
+        VUM.modify vec succ n
   VU.unsafeFreeze vec
-
-exactly10 :: [a] -> Bool
-exactly10 = Data.List.Match.equalLength (replicate 10 ())
-
-{- TODO: yup, we can do faster by avoiding divisorsSmall repeatly and count in the other direction -}
 
 result :: Int
 result =
-  getSum $
-    foldMap
-      (\n -> if n == 10 then 1 else 0)
-      $ VU.toList $ countSameDiffs (1000000-1)
+  getSum
+  . foldMap (\n -> if n == 10 then 1 else 0)
+  . VU.toList
+  $ countSameDiffs (1000000-1)
 
