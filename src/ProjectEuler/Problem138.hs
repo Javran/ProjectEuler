6a95db32c9f83bde536bb4be296f441eb4a660c8
Javran Cheng <Javran.C@gmail.com>
2019-12-02 21:55:15 -0800

    p138: solved.

--- src/ProjectEuler/Problem138.hs
+++ src/ProjectEuler/Problem138.hs
@@ -5,11 +5,12 @@ module ProjectEuler.Problem138
 import Data.Maybe
 import Math.NumberTheory.Powers
 import Control.Monad
+import Data.Word
 
 import ProjectEuler.Types
 
 problem :: Problem
-problem = pureProblem 138 Unsolved result
+problem = pureProblem 138 Solved result
 
 {-
   Idea:
@@ -34,12 +35,32 @@ problem = pureProblem 138 Unsolved result
     = (2 sqrt(5L^2 - 1) ∓ 4) / 5) (since L > 1, we know 2 sqrt(5L^2 - 1) > 4 and we want b > 0)
 
   which needs to be an integer.
+
+  (2 sqrt(5L^2 - 1) ∓ 4) === 0 (mod 5)
+  > 2 sqrt(5L^2 - 1) === ± 4 (mod 5)
+  > 3 * 2 sqrt(5L^2 - 1) === 3 * (± 4) (mod 5)
+  > sqrt(5L^2 - 1) === 2 or 3 (mod 5)
  -}
 
-result = take 6 $ mapMaybe (\l -> do
-                      z <- exactSquareRoot (5*l*l-1)
-                      let r = z `mod` 5
-                      guard $ r == 2 || r == 3
-                      pure l
-                  ) [1 :: Int ..]
+{-
+  Initial search: [1,17,305,5473,98209,1762289,31622993],
+  which leads me to: http://oeis.org/A007805
+ -}
+_doSearch :: [Int]
+_doSearch =
+  take 7
+  $ mapMaybe
+      (\l -> do
+          z <- exactSquareRoot (5*l*l-1)
+          let r = z `mod` 5
+          guard $ r == 2 || r == 3
+          pure l)
+      [1..]
 
+fibs :: [Word64]
+fibs = 0:1:zipWith (+) (tail fibs) fibs
+
+result :: Word64
+result = sum $ f <$> [1..12]
+  where
+    f n = fibs !! (6*n+3) `quot` 2
