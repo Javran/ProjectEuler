0ae0a92bff4a20ff346f96fef34c396077a91159
Javran Cheng <Javran.C@gmail.com>
2019-12-04 22:50:00 -0800

    p141: a bit more progress I guess.

--- src/ProjectEuler/Problem141.hs
+++ src/ProjectEuler/Problem141.hs
@@ -24,20 +24,21 @@ problem = pureProblem 141 Unsolved result
 
   Perhaps explore a bit more with each cases?
 
-  - case #1: k < r < q.
-    let r = k * z, q = k * z * z
-    n = q * k + r = k * z * z * k + k * z = k z (k z + 1) = x^2 (TODO)
+  - case #1: k < r < q => r * r = k * q
 
-  - case #2: r < k < q.
-    let k = r * z, q = r * z * z
-    n = q * k + r = r * z * z * r * z + r
-      = r (z^3 r + 1) = x^2 (TODO)
+    n = q * k + r = r * (r + 1) = x * x
 
-  - case #3: r < q < k.
-    let q = r * z, k = r * z * z
-    n = q * k + r = r * z * r * z * z + r
-      = r (z^3 r + 1) = x^2 (TODO)
-    For this case we end up having the same equation as case #2 to deal with.
+    Given that n > 0, there is no solution for when x > 0.
+
+  - case #2: r < k < q => r * q = k * k, q = k * k / r
+
+    n = q * k + r = k^3 / r + r, which needs to be a perfect square.
+
+    Since r is an integer, so must be k^3 / r, which means k^3 === 0 (mod r)
+
+  - case #3: r < q < k => r * k = q * q. k = q * q / r
+
+    n = q * k + r = q^3 / r + r, which needs to be a perfect square.
 
  -}
 
