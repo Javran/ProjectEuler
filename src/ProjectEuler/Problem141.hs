914caf5ff4be83f0ef66489dfefd1066e2e35577
Javran Cheng <Javran.C@gmail.com>
2019-12-05 22:03:44 -0800

    p141: k => d
    for some reason I was totally unaware that
    the problem does assign a variable name to this number.

--- src/ProjectEuler/Problem141.hs
+++ src/ProjectEuler/Problem141.hs
@@ -15,35 +15,35 @@ problem = pureProblem 141 Unsolved result
 {-
   Looks like this will be one of those difficult ones.
 
-  Say n = q * k + r.
+  Say n = q * d + r.
 
   - First, we don't want any of those to be zero,
-    since we want q,k,r to form geometric sequence (not in that particular order).
-    Therefore: q > 0 & k > 0 & r > 0
+    since we want q,d,r to form geometric sequence (not in that particular order).
+    Therefore: q > 0 & d > 0 & r > 0
   - Also 0 < r < q
 
-  So r < q is already in place, leaving k only 3 choices:
-  - k < r < q, in this case we can verify whether k * q == r * r
-  - r < k < q, verify that r * q == k * k
-  - r < q < k, verify that r * k == q * q
+  So r < q is already in place, leaving d only 3 choices:
+  - d < r < q, in this case we can verify whether d * q == r * r
+  - r < d < q, verify that r * q == d * d
+  - r < q < d, verify that r * d == q * q
 
   Perhaps explore a bit more with each cases?
 
-  - case #1: k < r < q => r * r = k * q
+  - case #1: d < r < q => r * r = d * q
 
-    n = q * k + r = r * (r + 1) = x * x
+    n = q * d + r = r * (r + 1) = x * x
 
     Given that n > 0, there is no solution for when x > 0.
 
-  - case #2: r < k < q => r * q = k * k, q = k * k / r
+  - case #2: r < d < q => r * q = d * d, q = d * d / r
 
-    n = q * k + r = k^3 / r + r, which needs to be a perfect square.
+    n = q * d + r = d^3 / r + r, which needs to be a perfect square.
 
-    Since r is an integer, so must be k^3 / r, which means k^3 === 0 (mod r)
+    Since r is an integer, so must be d^3 / r, which means d^3 === 0 (mod r)
 
-  - case #3: r < q < k => r * k = q * q. k = q * q / r
+  - case #3: r < q < d => r * d = q * q. d = q * q / r
 
-    n = q * k + r = q^3 / r + r, which needs to be a perfect square.
+    n = q * d + r = q^3 / r + r, which needs to be a perfect square.
 
   Given that the analysis of case #2 and case #3 ends up in basically the same
   equation: n = a^3 / r + r, and the problem limits n < 10^12,
@@ -67,12 +67,12 @@ result = sum xs
       let n = (aCube `quot` r) + r
       True <- pure $ n < 10 ^12
       Just _ <- pure $ exactSquareRoot n
-      [ (n, (r,k,q))
-        | let k = a, (q, 0) <- [(k * k) `quotRem` r]
-        , r < k && k < q
+      [ (n, (r,d,q))
+        | let d = a, (q, 0) <- [(d * d) `quotRem` r]
+        , r < d && d < q
         ] <> [
-        (n, (r,q,k))
+        (n, (r,q,d))
         | let q = a
-        , (k, 0) <- [(q * q) `quotRem` r]
-        , r < q && q < k
+        , (d, 0) <- [(q * q) `quotRem` r]
+        , r < q && q < d
         ]
