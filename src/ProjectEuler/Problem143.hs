aa36ac67d1fbb9232e50ef517e84fb0f180de5d0
Javran Cheng <Javran.C@gmail.com>
2019-12-07 23:32:46 -0800

    p143: correction on filtering.

--- src/ProjectEuler/Problem143.hs
+++ src/ProjectEuler/Problem143.hs
@@ -43,26 +43,27 @@ problem = pureProblem 143 Unsolved result
   - We can probably begin with searching primitive pairs gcd(a,b,c) = 1
     and scale them up to get a bunch of solutions.
 
-  - Now the problem is to find a strategy for searching edges.
-
-  - assume a >= b >= c, a triangle with 2*pi / 3 corner:
-
-    2*(2 + sqrt(3))*h < 120000, a = 2*sqrt(3)*h
-    a <= 55692
-    ... well, bounding the other two edges got to be tricky.
-    this is kind of complicated, let's just try searching from one..
+  - Now the problem is to find a strategy for searching edges...
+  - Another tricky problem: the bound is on p + q + r, I'm not sure how should I put bound on
+    triangles.
 
  -}
 
--- http://oeis.org/A229839 sounds related.
+-- http://oeis.org/A089025 looks promising for only primitives. (not exactly the same sequence it seems)
+-- http://oeis.org/A061281: this one seems to have some that doesn't qualify, but if we can find a way to
+-- test on numbers, this could work.
 result = LOrdered.nub $ fmap snd $ sortOn snd $ do
-  a <- [1..600]
+  a <- [1..2000]
   b <- [1..a]
-  c <- [1..b]
-  -- be a triangle
-  guard $ a < b + c && b < a + c && c < a + b
-  -- largest corner < 2 pi / 3
-  guard $ a*a + b*b + a*b > c * c
+  let gab = gcd a b
+      cMax = integerSquareRoot (a*a + b*b + a*b) - 1
+  -- lower bound encodes a < b + c => c > a - b
+  -- upper bound encodes  a*a + b*b + a*b > c * c (i.e. largest corner < 2 pi / 3)
+  c <- [a-b+1 .. b]
+  guard $ gcd gab c == 1
+  guard $ a*a + b*b + a*b > c * c && b*b + c*c + b*c > a*a && a*a + c*c + a*c > b*b
+  -- (i.e. largest corner < 2 pi / 3)
+  -- guard $ a < b + c -- no need of testing (b < a + c && c < a + b) because a >= b >= c >= 1
   let t :: Int
       t = a^!4+b^!4+c^!4-(a^!2-b^!2)^!2-(b^!2-c^!2)^!2-(a^!2-c^!2)^!2
   Just tR <- [exactSquareRoot (3*t)]
