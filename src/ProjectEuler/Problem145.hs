d031ea244c15c56b8381da8cdb5dc21d391d1939
Javran Cheng <Javran.C@gmail.com>
2019-12-09 00:07:06 -0800

    p145: got the right answer.

--- src/ProjectEuler/Problem145.hs
+++ src/ProjectEuler/Problem145.hs
@@ -2,11 +2,20 @@ module ProjectEuler.Problem145
   ( problem
   ) where
 
+import Petbox
+import Data.Monoid
+
 import ProjectEuler.Types
 
 problem :: Problem
 problem = pureProblem 145 Unsolved result
 
-result = ()
+isReversible :: Int -> Bool
+isReversible x = x `rem` 10 /= 0 && all odd xs
+  where
+    xs = intToDigitsRev (x + numReverseInBase 10 x)
 
+result :: Int
+result =
+  getSum $ foldMap (\x -> if isReversible x then 1 else 0) [1..1000000000-1]
 
