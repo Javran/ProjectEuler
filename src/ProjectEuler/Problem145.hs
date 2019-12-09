d2d3c712366724177330cd5e082a619372703612
Javran Cheng <Javran.C@gmail.com>
2019-12-09 00:43:47 -0800

    p145: notes, marked as solved.

--- src/ProjectEuler/Problem145.hs
+++ src/ProjectEuler/Problem145.hs
@@ -8,7 +8,19 @@ import Data.Monoid
 import ProjectEuler.Types
 
 problem :: Problem
-problem = pureProblem 145 Unsolved result
+problem = pureProblem 145 Solved result
+
+{-
+  Idea:
+
+  A brute force gets the answer.
+
+  Afterthoughts: turns out there is no 9-digit solutions,
+  which allows us to simply search 1~10^8-1 to get the right answer.
+
+  TODO: could do better, base on number of digits n mod 4.
+
+ -}
 
 isReversible :: Int -> Bool
 isReversible x = x `rem` 10 /= 0 && all odd xs
@@ -17,5 +29,7 @@ isReversible x = x `rem` 10 /= 0 && all odd xs
 
 result :: Int
 result =
-  getSum $ foldMap (\x -> if isReversible x then 1 else 0) [1..1000000000-1]
+  getSum
+  . foldMap (\x -> if isReversible x then 1 else 0) $
+    [1..10^!8-1]
 
