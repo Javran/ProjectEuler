d8035a209ada01797a9e716d2b479225892770d0
Javran Cheng <Javran.C@gmail.com>
2019-12-18 20:55:33 -0800

    p148: experiment.
    and few more notes that might be useful.

--- src/ProjectEuler/Problem148.hs
+++ src/ProjectEuler/Problem148.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE TupleSections #-}
 module ProjectEuler.Problem148
   ( problem
   ) where
 
+import Math.Combinat.Numbers.Sequences
 import ProjectEuler.Types
 
 problem :: Problem
-problem = pureProblem 148 Unsolved result
+problem = Problem 148 Unsolved run
 
 {-
   Idea: For Pascal's triangle,
@@ -24,8 +26,11 @@ problem = pureProblem 148 Unsolved result
   > An elementary property of binomial coefficients is that "n choose k"
   is divisible by a prime p for all 1 < k < n if and only if n is a power of p.
 
+  > A binomial coefficient binom {m}{n} is divisible by a prime p
+  if and only if at least one of the base p digits of n is greater than the corresponding digit of m.
+
  -}
 
-result = ()
+checkRow n = (n,) <$> filter ((== 0) . (`rem` 7) . binomial n) [0..n]
 
-
+run = mapM_ logT $ filter (not . null) $ fmap checkRow [1 :: Int ..100]
