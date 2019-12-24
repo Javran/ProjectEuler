3bcac310c01b9eb7440c8ab9c4e09d0ab04b6f96
Javran Cheng <Javran.C@gmail.com>
2019-12-23 22:46:22 -0800

    p149: improved performance.
    this time the internal state of Kadane's algorithm
    is stored and maintained in 2 unboxed vectors instead
    of going through all lines after the pre-processing.

--- src/ProjectEuler/Problem149.hs
+++ src/ProjectEuler/Problem149.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE NoMonomorphismRestriction, DeriveFunctor #-}
 module ProjectEuler.Problem149
   ( problem
   ) where
@@ -50,38 +50,46 @@ problem = pureProblem 149 Solved result
  -}
 
 result :: Int32
-result = maximum (findMax numTable 2000 (genLineCoords 2000))
+result = findMatrixMax 2000 cells
 
 type Coord = (Int,Int)
-type LineCoords = [] Coord -- coordinates that are in the same line.
 
-genLineCoords :: Int -> [] LineCoords
-genLineCoords l =
-    {-
-      note that for coordinate (r,c), we have 1 <= r,c <= l,
-      the index is started at one to make it consistent with problem's description.
-     -}
-    rows <> cols <> diags0 <> diags1
-  where
-    rows = [ [(r,c) | c <- [1..l]] | r <- [1..l] ]
-    cols = [ [(r,c) | r <- [1..l]] | c <- [1..l] ]
-    -- let's say diags0 is of the direction that keeps r + c = k a constant.
-    diags0 =
-      [ [ (r,c) | r <- [1..k-1], let c = k - r ] | k <- [2 .. l+1] ]
-      <> [ [ (r,c) | r <- [k-l..l], let c = k - r ] | k <- [l+2 .. l+l] ]
-    -- diags1 keeps r - c = k a constant. (TODO)
-    diags1 =
-      [ [ (r,c) | r <- [1 .. l+k], let c = r - k ] | k <- [1-l .. 0]]
-      <> [ [ (r,c) | r <- [k+1 .. l] , let c = r - k] | k <- [1 .. l-1] ]
+{-
+  Two vectors of the same length for running Kadane's algorithm.
+  - First vector holds current best value.
+  - Second vector holds current sum.
+ -}
+data Kadane v = Kadane v v deriving Functor
 
-getVal :: VUM.Unbox a => VU.Vector a -> Int -> Coord -> a
-getVal vec l (r,c) = vec VU.! ((r-1)*l + c)
+updateKadane (Kadane vecBest vecCur) i val = do
+  vBest <- VUM.read vecBest i
+  vCur <- VUM.read vecCur i
+  let (vBest', vCur') = kadaneAux (vBest, vCur) val
+  VUM.write vecBest i vBest'
+  VUM.write vecCur i vCur'
 
-findMax :: VU.Vector Int32 -> Int -> [LineCoords] -> [Int32]
-findMax vec l = fmap (maxSubArray . getVecLine)
-  where
-    getVecLine :: [(Int,Int)] -> [Int32]
-    getVecLine = fmap (getVal vec l)
+findMatrixMax :: Int -> [] (Coord, Int32) -> Int32
+findMatrixMax l cs = runST $ do
+  let initKadane sz = Kadane <$> VUM.new sz <*> VUM.new sz
+  rowsKn <- initKadane l
+  colsKn <- initKadane l
+  diags0Kn <- initKadane (l+l-1)
+  diags1Kn <- initKadane (l+l-1)
+  let base = l-1 -- this base allows diags1Kn's index to start from 0.
+  forM_ cs $ \((r,c), val) -> do
+    updateKadane rowsKn r val
+    updateKadane colsKn c val
+    updateKadane diags0Kn (r+c) val
+    updateKadane diags1Kn (r-c+base) val
+    pure ()
+  let getMax (Kadane mvec _) = do
+        vec <- VU.unsafeFreeze mvec
+        pure (maximum (VU.toList vec))
+  rowsMax <- getMax rowsKn
+  colsMax <- getMax colsKn
+  diags0Max <- getMax diags0Kn
+  diags1Max <- getMax diags1Kn
+  pure $ maximum [rowsMax, colsMax, diags0Max, diags1Max]
 
 {-
   Kadane's algorithm to compute sum of adjacent numbers.
@@ -89,13 +97,16 @@ findMax vec l = fmap (maxSubArray . getVecLine)
   the algorithm will return 0, which is fine for our case, because
   there are definitely positive numbers in our array (for example, s_100 = 86613)
  -}
-maxSubArray :: (Foldable t, Num a, Ord a) => t a -> a
-maxSubArray = fst . foldl' kadaneAux (0, 0)
-  where
-    kadaneAux (bestSum, prevSum) curVal = (bestSum', curSum)
+kadaneAux :: (Num a, Ord a) => (a, a) -> a -> (a, a)
+kadaneAux (bestSum, prevSum) curVal = (bestSum', curSum)
       where
         curSum = max 0 (prevSum + curVal)
         bestSum' = max bestSum curSum
+
+cells :: [] (Coord, Int32)
+cells = zip [ (r,c) | r <- [0..1999], c <- [0..1999] ] vals
+  where
+    _ : vals = VU.toList numTable
 
 numTable :: VU.Vector Int32
 numTable = runST $ do
