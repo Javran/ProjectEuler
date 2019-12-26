331b5d63a9c6fa09c86470fb73a93a9a900e4b20
Javran Cheng <Javran.C@gmail.com>
2019-12-25 22:19:18 -0800

    p150: draft of the plan.

--- src/ProjectEuler/Problem150.hs
+++ src/ProjectEuler/Problem150.hs
@@ -10,11 +10,21 @@ problem = pureProblem 150 Unsolved result
 {-
   Idea:
 
-  - We have 500500 numbers, that's in total 1000 lines.
-  - I suspect we can do something similiar to what we
-    have done in Problem149.
-  - I feel we should be able to get any sub-triangle sum
-    in an efficient manner at the cost of some pre-processing.
+  Note that all triangles that we want to search can be uniquely represented
+  by two coordinates on the same row - these two coordinates will be the base
+  of that triangle. Since this triangle is equilateral and we've known
+  the length and position of its base, we have everything we need for our task.
+
+  let (r,c) be coordinates in this large triangle, 0 <= r < 1000, 0 <= c <= r.
+
+  Now we can maintain two arrays as we approach the final answer:
+
+  - a rowSums[i] array that records accumulative sum between 0..i of that row,
+    for a O(1) range-sum lookup.
+  - a triSums[i,j] array, in which i <= j, that records sum of the triangles
+    whose base is from i to j of that row. it should be possible to
+    reuse triSum of previous row so we never go back to already processed rows
+    to do the summation.
 
  -}
 
